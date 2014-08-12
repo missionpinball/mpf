0daf113ffcf982909d2966f9ce74493f3ec11601
Brian Madden <toomanybrians@gmail.com>
2014-08-12 13:37:48 -0700

    Update conf.py

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'napoleon'
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
