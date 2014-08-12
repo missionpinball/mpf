4819600857d539ca0a877d1c16e701e11562f882
Brian Madden <toomanybrians@gmail.com>
2014-08-12 13:22:54 -0700

    Update conf.py
    Changing the path for napoleon

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
