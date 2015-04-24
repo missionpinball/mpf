09b221832adb219bfee70325d99e9a45fadd6ae9
toomanybrians <brian@missionpinball.com>
2015-04-23 20:51:28 -0700

    docstrings cleanup

--- mpf/devices/matrix_light.py
+++ mpf/devices/matrix_light.py
@@ -37,7 +37,8 @@ class MatrixLight(Device):
         # some things later.
         self.config['number_str'] = str(config['number']).upper()
 
-        self.hw_driver, self.number = self.machine.platform.configure_matrixlight(self.config)
+        self.hw_driver, self.number = (
+            self.machine.platform.configure_matrixlight(self.config))
 
         self.registered_handlers = []
 
