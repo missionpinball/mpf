d570b41bb0b2c795daef7c240d27c47b4acdb77b
toomanybrians <brian@missionpinball.com>
2015-05-07 09:50:44 -0700

    Bug Fix: Event manager crash on some iterations
    Event manager would sometimes crash if event queues were changed while
    the event they were tied to was active

--- mpf/system/events.py
+++ mpf/system/events.py
@@ -8,6 +8,7 @@
 
 import logging
 from collections import deque
+from copy import copy
 import uuid
 
 
@@ -373,7 +374,7 @@ class EventManager(object):
             self.queue.append((event, ev_type, callback, kwargs))
 
             self.log.debug("================== ACTIVE EVENTS ==================")
-            for event in self.queue:
+            for event in copy(self.queue):
                 self.log.debug("%s, %s, %s, %s", event[0], event[1], event[2],
                                event[3])
             self.log.debug("==================================================")
