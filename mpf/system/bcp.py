ec21c764d35fbb4f18bfd034e70cefeda8ce8d5a
toomanybrians <brian@missionpinball.com>
2015-06-15 15:43:01 -0700

    Bug Fix: Better processing of __all__ player variable setting in BCP
    MPF rev to 0.19.0a6

--- mpf/system/bcp.py
+++ mpf/system/bcp.py
@@ -174,13 +174,9 @@ class BCP(object):
 
             self.send_player_vars = True
 
-            if (type(self.config['player_variables']) is str and
-                    self.config['player_variables'] == '__all__'):
-                self.filter_player_events = False
+            self.config['player_variables'] = (
+                Config.string_to_list(self.config['player_variables']))
 
-            else:
-                self.config['player_variables'] = (
-                    Config.string_to_list(self.config['player_variables']))
 
         self._setup_player_monitor()
         self.register_mpfmc_trigger_events(self.machine.config)
