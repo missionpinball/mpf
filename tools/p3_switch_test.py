3d48120c52d9eed3f8802fbec49c4cd20cdebae3
toomanybrians <brian@missionpinball.com>
2015-06-23 18:31:38 -0700

    Added P3 Switch Test tool

---
+++ tools/p3_switch_test.py
@@ -0,0 +1,11 @@
+import pinproc
+
+proc = pinproc.PinPROC(pinproc.normalize_machine_type('pdb'))
+
+for switch, state in enumerate(proc.switch_get_states()):
+
+    if not switch % 16:
+        print('')
+        print('SW-16 Board Address {0}'.format(switch / 16))
+
+    print('Switch {0} State {1}'.format(switch, state))
