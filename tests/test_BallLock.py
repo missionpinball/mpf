8517e2d737d6a05308d46de5728e6d8cf7477ca9
Jan Kantert <jan-mpf@kantert.net>
2015-10-26 16:19:58 +0100

    test ball_lock at game end

--- tests/test_BallLock.py
+++ tests/test_BallLock.py
@@ -38,14 +38,17 @@ class TestBallLock(MpfTestCase):
 
         self.machine.events.add_handler('balldevice_captured_from_playfield', self._captured_from_pf)
         self.machine.events.add_handler('balldevice_1_ball_missing', self._missing_ball)
+        self.machine.events.add_handler('balldevice_test_launcher_ball_request', self._requesting_ball)
         self._enter = 0
         self._captured = 0
         self._missing = 0
+        self._requesting = 0
 
         # add an initial ball to trough
         self.machine.switch_controller.process_switch("s_ball_switch1", 1)
+        self.machine.switch_controller.process_switch("s_ball_switch2", 1)
         self.advance_time_and_run(1)
-        self.assertEquals(1, self._captured)
+        self.assertEquals(2, self._captured)
         self._captured = 0
         self.assertEquals(0, playfield.balls)
 
@@ -53,14 +56,19 @@ class TestBallLock(MpfTestCase):
         coil1.pulse = MagicMock()
         coil2.pulse = MagicMock()
         coil3.pulse = MagicMock()
-        self.assertEquals(1, trough.count_balls())
+        self.assertEquals(2, trough.count_balls())
         assert not coil1.pulse.called
         assert not coil2.pulse.called
         assert not coil3.pulse.called
+        self.machine.ball_controller.num_balls_known = 2
 
-        # request an ball
-        playfield.add_ball()
+        # start a game
+        self.machine.switch_controller.process_switch("s_start", 1)
+        self.advance_time_and_run(0.1)
+        self.machine.switch_controller.process_switch("s_start", 0)
         self.advance_time_and_run(1)
+        self.assertEquals(1, self._requesting)
+        self._requesting = 0
 
         # trough ejects
         coil1.pulse.assert_called_once_with()
@@ -69,7 +77,7 @@ class TestBallLock(MpfTestCase):
 
         self.machine.switch_controller.process_switch("s_ball_switch1", 0)
         self.advance_time_and_run(1)
-        self.assertEquals(0, trough.count_balls())
+        self.assertEquals(1, trough.count_balls())
 
 
         # launcher receives and ejects
@@ -81,7 +89,7 @@ class TestBallLock(MpfTestCase):
         coil2.pulse.assert_called_once_with()
         assert not coil3.pulse.called
 
-        # player shoots the ball
+        # launcher shoots the ball
         self.machine.switch_controller.process_switch("s_ball_switch_launcher", 0)
         self.advance_time_and_run(1)
         self.assertEquals(0, launcher.count_balls())
@@ -95,27 +103,87 @@ class TestBallLock(MpfTestCase):
         self.assertEquals(0, self._captured)
         self.assertEquals(0, self._missing)
 
+        coil1.pulse = MagicMock()
+        coil2.pulse = MagicMock()
+        coil3.pulse = MagicMock()
 
-        # ball enters lock but lock is not active. it will eject
+        # ball enters lock 
         self.machine.switch_controller.process_switch("s_ball_switch_lock1", 1)
         self.advance_time_and_run(1)
         self.assertEquals(1, lock.count_balls())
 
+        # it will request another ball
         coil1.pulse.assert_called_once_with()
-        coil2.pulse.assert_called_once_with()
-        coil3.pulse.assert_called_once_with()
+        assert not coil2.pulse.called
+        assert not coil3.pulse.called
 
         self.assertEquals(0, playfield.balls)
         self.assertEquals(1, self._captured)
         self.assertEquals(0, self._missing)
-        self.assertEquals(0, lock_logic.balls_locked)
+        self.assertEquals(1, lock_logic.balls_locked)
         self._captured = 0
+        self.assertEquals(1, self._requesting)
+        self._requesting = 0
 
-        self.machine.switch_controller.process_switch("s_ball_switch_lock1", 0)
+        self.machine.switch_controller.process_switch("s_ball_switch2", 0)
+        self.advance_time_and_run(1)
+        self.assertEquals(0, trough.count_balls())
+
+
+        # launcher receives and ejects
+        self.machine.switch_controller.process_switch("s_ball_switch_launcher", 1)
+        self.advance_time_and_run(1)
+        self.assertEquals(1, launcher.count_balls())
+
+        coil1.pulse.assert_called_once_with()
+        coil2.pulse.assert_called_once_with()
+        assert not coil3.pulse.called
+
+        # launcher shoots the ball
+        self.machine.switch_controller.process_switch("s_ball_switch_launcher", 0)
+        self.advance_time_and_run(1)
+        self.assertEquals(0, launcher.count_balls())
+
+        self.machine.switch_controller.process_switch("s_playfield_active", 1)
+        self.advance_time_and_run(0.1)
+        self.machine.switch_controller.process_switch("s_playfield_active", 0)
         self.advance_time_and_run(1)
 
         self.assertEquals(1, playfield.balls)
         self.assertEquals(0, self._captured)
         self.assertEquals(0, self._missing)
+
+        # ball drains and game ends
+        self.machine.switch_controller.process_switch("s_ball_switch1", 1)
+        self.advance_time_and_run(1)
+
+        self.assertEquals(0, playfield.balls)
+        self.assertEquals(1, self._captured)
+        self.assertEquals(0, self._missing)
+        self._captured = 0
+
+        # lock should eject all balls
+        coil1.pulse.assert_called_once_with()
+        coil2.pulse.assert_called_once_with()
+        coil3.pulse.assert_called_once_with()
+
+        self.machine.switch_controller.process_switch("s_ball_switch_lock1", 0)
+        self.advance_time_and_run(1)
+        self.assertEquals(0, lock.count_balls())
         self.assertEquals(0, lock_logic.balls_locked)
+        self.assertEquals(1, playfield.balls)
+        self.assertEquals(0, self._captured)
+        self.assertEquals(0, self._missing)
+
+        # ball also drains
+        self.machine.switch_controller.process_switch("s_ball_switch2", 1)
+        self.advance_time_and_run(1)
+        self.assertEquals(0, playfield.balls)
+        self.assertEquals(1, self._captured)
+        self.assertEquals(0, self._missing)
+        self.assertEquals(0, self._requesting)
+
+
+
+
 
