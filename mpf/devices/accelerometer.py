f9f7a2f453c07ea75205381cc39ff5f0f425cd5b
Jan Kantert <jan-mpf@kantert.net>
2015-12-26 17:32:16 +0100

    Cleanup Accelerometer

--- mpf/devices/accelerometer.py
+++ mpf/devices/accelerometer.py
@@ -1,4 +1,5 @@
 """ Contains the Accelerometer """
+# Written by Jan Kantert
 
 import time
 import math
@@ -37,9 +38,13 @@ class Accelerometer(Device):
         return math.sqrt(x*x + y*y + z*z)
 
     def _calculate_angle(self, x1, y1, z1, x2, y2, z2):
-        return math.acos((x1*x2 + y1*y2 + z1*z2) /
-                (self._calculate_vector_length(x1, y1, z1) *
-                self._calculate_vector_length(x2, y2, z2)))
+        dividor = (self._calculate_vector_length(x1, y1, z1) *
+                self._calculate_vector_length(x2, y2, z2))
+
+        if dividor == 0:
+            return 0
+
+        return math.acos((x1*x2 + y1*y2 + z1*z2) / dividor)
 
     def update_acceleration(self, x, y, z):
         self.value = (x, y, z)
@@ -58,30 +63,40 @@ class Accelerometer(Device):
                             self.history[2] * alpha + z * (1-alpha))
 
         self._handle_hits(dx, dy, dz)
-        self._handle_level(x, y, z)
+        self._handle_level()
 
-    def _handle_level(self, x, y ,z):
-        deviation_total = self._calculate_angle(self.config['level_x'],
+    def get_level_xyz(self):
+        return self._calculate_angle(self.config['level_x'],
                                 self.config['level_y'], self.config['level_z'],
-                                x, y, z)
-        deviation_x = self._calculate_angle(0, self.config['level_y'],
-                                self.config['level_z'], 0, y, z)
-        deviation_y = self._calculate_angle(self.config['level_x'], 0,
-                                self.config['level_z'], x, 0, z)
+                                self.value[0], self.value[1], self.value[2])
+
+    def get_level_xz(self):
+        return self._calculate_angle(self.config['level_x'],
+                                0.0, self.config['level_z'],
+                                self.value[0], 0.0, self.value[2])
+
+    def get_level_yz(self):
+        return self._calculate_angle(0.0,
+                                self.config['level_y'], self.config['level_z'],
+                                0.0, self.value[1], self.value[2])
+
+    def _handle_level(self):
+        deviation_xyz = self.get_level_xyz()
+        deviation_xz = self.get_level_xz()
+        deviation_yz = self.get_level_yz()
+
         for max_deviation in self.config['level_limits']:
-            if deviation_total/math.pi*180 > max_deviation:
+            if deviation_xyz/math.pi*180 > max_deviation:
                 self.log.debug("Deviation x: %s, y: %s, total: %s",
-                    deviation_x/math.pi*180,
-                    deviation_y/math.pi*180,
-                    deviation_total/math.pi*180)
+                    deviation_xz/math.pi*180,
+                    deviation_yz/math.pi*180,
+                    deviation_xyz/math.pi*180)
                 self.machine.events.post(self.config['level_limits'][max_deviation],
-                    deviation_total=deviation_total,
-                    deviation_x=deviation_x,
-                    deviation_y=deviation_y)
+                    deviation_xyz=deviation_xyz,
+                    deviation_xz=deviation_xz,
+                    deviation_yz=deviation_yz)
 
-            
-
-    def _handle_hits(self, dx, dy ,dz):
+    def _handle_hits(self, dx, dy, dz):
         acceleration = self._calculate_vector_length(dx, dy, dz)
         for min_acceleration in self.config['hit_limits']:
             if acceleration > min_acceleration:
