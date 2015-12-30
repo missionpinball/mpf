f9de319e875fdb295fb2a10e397c0ff0de46a558
Brian Madden <brian@missionpinball.com>
2015-12-29 23:48:48 -0500

    Make mode entries from config not case sensitive

--- mpf/system/mode_controller.py
+++ mpf/system/mode_controller.py
@@ -83,8 +83,10 @@ class ModeController(object):
         # Loads the modes from the modes: section of the machine configuration
         # file.
 
+        # todo make a config file validator entry for lowercase values
+
         for mode in set(self.machine.config['modes']):
-            self.machine.modes.append(self._load_mode(mode))
+            self.machine.modes.append(self._load_mode(mode.lower()))
 
     def _load_mode(self, mode_string):
         """Loads a mode, reads in its config, and creates the Mode object.
@@ -145,7 +147,7 @@ class ModeController(object):
         # If a custom 'code' setting exists, first look in the machine folder
         # for it, and if it's not there, then look in mpf/modes for it.
 
-        if 'code' in config['mode']:
+        if 'mode' in config and 'code' in config['mode']:
             mode_code_file = os.path.join(self.machine.machine_path,
                 self.machine.config['mpf']['paths']['modes'],
                 mode_string,
