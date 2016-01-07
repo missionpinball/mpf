f9c4e93f17b31a3eaacb0bf97c3824a4596c9b09
toomanybrians <brian@missionpinball.com>
2016-01-07 10:08:28 -0800

    Remove MC from MPF package, MPF version rev to .30

--- mpf/kmc/uix/mpf_widget.py
+++
@@ -1,4 +0,0 @@
-
-
-class MpfWidget(object):
-    pass
--- tests/test_Kmc.py
+++
@@ -1,9 +0,0 @@
-
-from .KmcTestCase import KmcTestCase
-
-
-class TestKmc(KmcTestCase):
-
-    def test_kmc_start(self):
-        from kivy.core.window import Window
-        print(Window.size)
