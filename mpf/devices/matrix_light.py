8a54170b136b250797750dc4c66a135843387223
Quinn Capen <qcapen@gmail.com>
2016-01-28 08:56:25 -0700

    Added fading to MatrixLight devices

--- mpf/devices/matrix_light.py
+++ mpf/devices/matrix_light.py
@@ -1,7 +1,8 @@
 """ Contains the MatrixLight parent classes. """
 
 from mpf.system.device import Device
-
+from mpf.system.tasks import Task
+import time
 
 class MatrixLight(Device):
     """ Represents a light connected to a traditional lamp matrix in a pinball
@@ -33,11 +34,24 @@ class MatrixLight(Device):
 
         self.state = {  # current state of this light
             'brightness': 0,
-            'priority': 0}
+            'priority': 0,
+            'destination_brightness': 0,
+            'destination_time': 0.0,
+            'start_brightness': 0,
+            'start_time': 0.0
+        }
 
         self.cache = {  # cached state of last manual command
             'brightness': 0,
-            'priority': 0}
+            'priority': 0,
+            'destination_brightness': 0,
+            'destination_time': 0.0,
+            'start_brightness': 0,
+            'start_time': 0.0
+        }
+
+        self.fade_task = None
+        self.fade_in_progress = False
 
         # set up the X, Y coordinates
         self.x = None
@@ -49,8 +63,7 @@ class MatrixLight(Device):
         if 'y' in config:
             self.y = config['y']
 
-    def on(self, brightness=255, fade_ms=0, start_brightness=None,
-           priority=0, cache=True, force=False):
+    def on(self, brightness=255, fade_ms=0, priority=0, cache=True, force=False):
         """Turns on this matrix light.
 
         Args:
@@ -58,8 +71,8 @@ class MatrixLight(Device):
                 and 255. 0 is off. 255 is full on. Note that intermediary
                 values are not yet implemented, so 0 is off, anything from
                 1-255 is full on.
-            fade_ms: Not yet implemented
-            start_brightness: Not yet implemented.
+            fade_ms: The number of milliseconds to fade from the current
+                brightness level to the desired brightness level.
             priority: The priority of the incoming request. If this priority is
                 lower than the current cached priority, this on command will
                 have no effect. (Unless force=True)
@@ -85,22 +98,45 @@ class MatrixLight(Device):
             for handler in self.registered_handlers:
                 handler(light_name=self.name, brightness=brightness)
 
-        self.state['brightness'] = brightness
+        current_time = time.time()
+
+        # update our state
         self.state['priority'] = priority
+
+        if fade_ms:
+            self.state['fade_ms'] = fade_ms
+            self.state['destination_brightness'] = brightness
+            self.state['start_brightness'] = self.state['brightness']
+            self.state['start_time'] = current_time
+            self.state['destination_time'] = current_time + (fade_ms / 1000.0)
+            self._setup_fade()
+
+            if self.debug:
+                print("we have a light fade to set up")
+
+        else:
+            self.state['brightness'] = brightness
+
+            if self.debug:
+                self.log.debug("Setting Brightness: %s", brightness)
+
+            self.hw_driver.on(brightness)
 
         if cache:
             self.cache['brightness'] = brightness
+            self.cache['start_brightness'] = self.state['brightness']
+            self.cache['destination_brightness'] = self.state['destination_brightness']
+            self.cache['start_time'] = current_time
+            self.cache['destination_time'] = self.state['destination_time']
             self.cache['priority'] = priority
-
-        self.hw_driver.on(brightness, fade_ms, start_brightness)
+            self.cache['fade_ms'] = fade_ms
 
     def off(self, fade_ms=0, priority=0, cache=True, force=False):
-        self.on(brightness=0, fade_ms=fade_ms, priority=priority, cache=cache,
-                force=force)
         """Turns this light off.
 
         Args:
-            fade_ms: Not yet implemented
+            fade_ms: The number of milliseconds to fade from the current
+                brightness level to a brightness level of 0 (off).
             priority: The priority of the incoming request. If this priority is
                 lower than the current cached priority, this on command will
                 have no effect. (Unless force=True)
@@ -111,6 +147,12 @@ class MatrixLight(Device):
                 regardless of the incoming and current priority. Default is
                 False.
         """
+        self.on(brightness=0, fade_ms=fade_ms, priority=priority, cache=cache,
+                force=force)
+
+    def get_state(self):
+        """Returns the current state of this light"""
+        return self.state
 
     def add_handler(self, callback):
         """Registers a handler to be called when this light changes state."""
@@ -137,3 +179,67 @@ class MatrixLight(Device):
                 priority=self.cache['priority'],
                 force=True,
                 cache=True)
+
+    def _setup_fade(self):
+        """
+        Sets up the fade task for this LED.
+        Returns: None
+        """
+        self.fade_in_progress = True
+
+        if not self.fade_task:
+            if self.debug:
+                print("setting up fade task")
+            self.fade_task = Task.create(self._fade_task)
+        elif self.debug:
+                print("already have a fade task")
+
+    def _fade_task(self):
+        """
+        Task that performs a fade from the current brightness to the target brightness
+        over the specified fade time.
+        Returns: None
+        """
+        while self.fade_in_progress:
+
+            if self.debug:
+                print("fade_in_progress fade_task")
+                print("state", self.state)
+
+            state = self.state
+
+            # figure out the ratio of how far along we are
+            ratio = ((time.time() - state['start_time']) /
+                     (state['destination_time'] - state['start_time']))
+
+            if self.debug:
+                print("ratio", ratio)
+
+            if ratio >= 1.0:  # fade is done
+                self.fade_in_progress = False
+                set_cache = True
+                new_brightness = state['destination_brightness']
+
+            else:
+                set_cache = False
+                new_brightness = state['start_brightness'] + int((state['destination_brightness'] -
+                                                                  state['start_brightness']) * ratio)
+
+            if self.debug:
+                print("new brightness", new_brightness)
+
+            self.on(brightness=new_brightness, fade_ms=0, priority=state['priority'], cache=set_cache)
+
+            yield
+
+        if self.debug:
+            print("fade_in_progress just ended")
+            print("killing fade task")
+
+        self.fade_task = None
+        raise StopIteration()
+
+    def _kill_fade(self):
+        self.fade_in_progress = False
+        self.on(brightness=self.state['destination_brightness'],
+                fade_ms=0, priority=self.state['priority'], cache=True)
