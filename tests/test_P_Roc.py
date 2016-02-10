86a17d220938cfb435e7b95d69ecab076392c68e
Jan Kantert <jan-mpf@kantert.net>
2016-02-10 23:42:23 +0100

    more and better P-Roc tests

--- tests/test_P_Roc.py
+++ tests/test_P_Roc.py
@@ -1,50 +1,55 @@
-import unittest
-
-from mpf.core.machine import MachineController
 from tests.MpfTestCase import MpfTestCase
 from mock import MagicMock, call
 from mpf.platform import p_roc
 
+class MockPinProcModule(MagicMock):
+    DriverCount = 256
+
+    EventTypeAccelerometerIRQ = 11
+    EventTypeAccelerometerX = 8
+    EventTypeAccelerometerY = 9
+    EventTypeAccelerometerZ = 10
+    EventTypeBurstSwitchClosed = 7
+    EventTypeBurstSwitchOpen = 6
+    EventTypeDMDFrameDisplayed = 5
+    EventTypeSwitchClosedDebounced = 1
+    EventTypeSwitchClosedNondebounced = 3
+    EventTypeSwitchOpenDebounced = 2
+    EventTypeSwitchOpenNondebounced = 4
+
+    MachineTypeCustom = 1
+    MachineTypeInvalid = 0
+    MachineTypePDB = 7
+    MachineTypeSternSAM = 6
+    MachineTypeSternWhitestar = 5
+    MachineTypeWPC = 3
+    MachineTypeWPC95 = 4
+    MachineTypeWPCAlphanumeric = 2
+
+    SwitchCount = 255
+    SwitchNeverDebounceFirst = 192
+    SwitchNeverDebounceLast = 255
 
 class TestPRoc(MpfTestCase):
     def getConfigFile(self):
         return 'config.yaml'
 
     def getMachinePath(self):
-        return '../tests/machine_files/p3_roc/'
-
-#    def getOptions(self):
-#        options = super().getOptions()
-#        options['force_platform'] = False
-#        return options
+        return '../tests/machine_files/p_roc/'
 
     def get_platform(self):
         return 'p_roc'
 
-    def _decode(self, type, num):
-        if num == "A1-B1-2":
-            return 26
-        elif num == "A1-B1-3":
-            return 27
-        elif num == "A1-B0-7":
-            return 23
-        elif num == "A0-B1-0":
-            return 8
-        elif num == "A2-B1-0":
-            return 40
-        else:
-            raise AssertionError("unexpected decode called " + num)
-
     def setUp(self):
         p_roc.pinproc_imported = True
-        p_roc.pinproc = MagicMock()
-        p_roc.pinproc.DriverCount = 256
-        p_roc.pinproc.SwitchNeverDebounceFirst = 192
-        p_roc.pinproc.EventTypeSwitchClosedDebounced = 1
-        p_roc.pinproc.EventTypeSwitchOpenDebounced = 2
-        p_roc.pinproc.decode = self._decode
+        p_roc.pinproc = MockPinProcModule()
+        pinproc = MagicMock()
+        p_roc.pinproc.PinPROC = MagicMock(return_value=pinproc)
+        p_roc.pinproc.normalize_machine_type = MagicMock(return_value=7)
+        p_roc.pinproc.decode = None # no decode with PDB boards
         p_roc.pinproc.driver_state_pulse = MagicMock(
             return_value="driver_state_pulse")
+        pinproc.switch_get_states = MagicMock(return_value=[0,1,0,0,0,0,0,0,0,0,0])
         super().setUp()
 
     def test_pulse(self):
@@ -52,8 +57,9 @@ class TestPRoc(MpfTestCase):
         self.machine.coils.c_test.pulse()
         # A1-B1-2 -> address 16 + 8 + 2 = 26 in P3-Roc
         # for 23ms (from config)
+        number = self.machine.coils.c_test.hw_driver.number
         self.machine.coils.c_test.hw_driver.proc.driver_pulse.assert_called_with(
-            26, 23)
+            number, 23)
         assert not self.machine.coils.c_test.hw_driver.proc.driver_schedule.called
 
     def test_enable_exception(self):
@@ -63,8 +69,9 @@ class TestPRoc(MpfTestCase):
 
     def test_allow_enable(self):
         self.machine.coils.c_test_allow_enable.enable()
+        number = self.machine.coils.c_test_allow_enable.hw_driver.number
         self.machine.coils.c_test.hw_driver.proc.driver_schedule.assert_called_with(
-                number=27, cycle_seconds=0, now=True, schedule=0xffffffff)
+                number=number, cycle_seconds=0, now=True, schedule=0xffffffff)
 
     def test_hw_rule_pulse(self):
         self.machine.autofires.ac_slingshot_test.enable()
@@ -72,6 +79,11 @@ class TestPRoc(MpfTestCase):
                 40, 'closed_nondebounced',
                 {'notifyHost': False, 'reloadActive': False},
                 ["driver_state_pulse"], False)
+
+    def test_initial_switches(self):
+        self.assertFalse(self.machine.switch_controller.is_active("s_test"))
+        self.assertFalse(self.machine.switch_controller.is_active("s_test_000"))
+        self.assertTrue(self.machine.switch_controller.is_active("s_test_001"))
 
     def test_switches(self):
         self.assertFalse(self.machine.switch_controller.is_active("s_test"))
