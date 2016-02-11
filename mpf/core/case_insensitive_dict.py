874b2878022aa230c62f03c870ebd9b928e7d9ff
Jan Kantert <jan-mpf@kantert.net>
2016-02-12 00:11:54 +0100

    fix CS and remove deprecated method

--- mpf/core/case_insensitive_dict.py
+++ mpf/core/case_insensitive_dict.py
@@ -1,8 +1,9 @@
 # Based on this: http://stackoverflow.com/questions/2082152/case-insensitive-dictionary
 
+
 class CaseInsensitiveDict(dict):
-    @classmethod
-    def _k(cls, key):
+    @staticmethod
+    def lower(key):
         return key.lower() if isinstance(key, str) else key
 
     def __init__(self, *args, **kwargs):
@@ -10,32 +11,29 @@ class CaseInsensitiveDict(dict):
         self._convert_keys()
 
     def __getitem__(self, key):
-        return super().__getitem__(self.__class__._k(key))
+        return super().__getitem__(self.__class__.lower(key))
 
     def __setitem__(self, key, value):
-        super().__setitem__(self.__class__._k(key), value)
+        super().__setitem__(self.__class__.lower(key), value)
 
     def __delitem__(self, key):
-        return super().__delitem__(self.__class__._k(key))
+        return super().__delitem__(self.__class__.lower(key))
 
     def __contains__(self, key):
-        return super().__contains__(self.__class__._k(key))
-
-    def has_key(self, key):
-        return super().has_key(self.__class__._k(key))
+        return super().__contains__(self.__class__.lower(key))
 
     def pop(self, key, *args, **kwargs):
-        return super().pop(self.__class__._k(key), *args, **kwargs)
+        return super().pop(self.__class__.lower(key), *args, **kwargs)
 
     def get(self, key, *args, **kwargs):
-        return super().get(self.__class__._k(key), *args, **kwargs)
+        return super().get(self.__class__.lower(key), *args, **kwargs)
 
     def setdefault(self, key, *args, **kwargs):
-        return super().setdefault(self.__class__._k(key), *args, **kwargs)
+        return super().setdefault(self.__class__.lower(key), *args, **kwargs)
 
-    def update(self, E=None, **F):
-        super().update(self.__class__(E))
-        super().update(self.__class__(**F))
+    def update(self, e=None, **f):
+        super().update(self.__class__(e))
+        super().update(self.__class__(**f))
 
     def _convert_keys(self):
         for k in list(self.keys()):
