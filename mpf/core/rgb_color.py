bf89efbc55555a00c0117ab3ce7a9b0b22a61ccb
Jan Kantert <jan-mpf@kantert.net>
2016-02-22 03:10:04 +0100

    CS fix

--- mpf/core/rgb_color.py
+++ mpf/core/rgb_color.py
@@ -166,7 +166,7 @@ class RGBColor(object):
         """ Initialization """
 
         if isinstance(color, RGBColor):
-            self._color = color._color
+            self._color = color.rgb
         elif isinstance(color, str):
             self._color = RGBColor.string_to_rgb(color)
         else:
@@ -498,7 +498,7 @@ class RGBColorCorrectionProfile(object):
         # Default lookup table values (linear)
         self._lookup_table = []
 
-        for channel in range(3):
+        for dummy_channel in range(3):
             self._lookup_table.append([i for i in range(256)])
 
     def generate_from_parameters(self, gamma=2.5, whitepoint=(1.0, 1.0, 1.0),
