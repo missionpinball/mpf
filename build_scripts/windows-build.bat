98fedafb1c1c24c497c1d42df89ff075f02c49d2
toomanybrians <brian@missionpinball.com>
2016-03-15 18:34:32 -0700

    Updated build script

--- build_scripts/windows-build.bat
+++ build_scripts/windows-build.bat
@@ -1,7 +1,8 @@
-python -m pip install -U setuptools wheel pip mock
+python -m pip install -U setuptools wheel pip mock --retries 20 --timeout 60
 git clone file:///z/git/mpf c:\mpf
 c:
 cd /mpf
-pip install .
+pip install . --retries 20 --timeout 60
 python -m unittest discover -s mpf/tests
-python setup.py bdist_wheel  --dist-dir=%~dp0%/wheels
+python setup.py bdist_wheel --dist-dir=%~dp0%/dist
+python setup.py sdist --dist-dir=%~dp0%/dist  --formats=gztar
