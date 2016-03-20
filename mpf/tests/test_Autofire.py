0060c2fd29c3a2afa9145d533eafa160bf7d7a1c
Jan Kantert <jan-mpf@kantert.net>
2016-03-21 00:49:16 +0100

    add test for autofire + platform

---
+++ mpf/tests/test_Autofire.py
@@ -0,0 +1,44 @@
+from mock import MagicMock
+from mpf.tests.MpfTestCase import MpfTestCase
+
+
+class TestAutofire(MpfTestCase):
+    def getConfigFile(self):
+        return 'config.yaml'
+
+    def getMachinePath(self):
+        return 'tests/machine_files/autofire/'
+
+    def test_hw_rule_pulse(self):
+        self.machine.default_platform.write_hw_rule = MagicMock()
+        self.machine.autofires.ac_test.enable()
+
+        self.assertEqual(
+            (self.machine.switches.s_test,
+             1,
+             self.machine.coils.c_test,
+             'pulse',
+             False,
+             False), self.machine.default_platform.write_hw_rule._mock_call_args_list[0][0])
+
+        self.machine.default_platform.clear_hw_rule = MagicMock()
+        self.machine.autofires.ac_test.disable()
+
+        self.machine.default_platform.clear_hw_rule.assert_called_once_with("s_test")
+
+    def test_hw_rule_pulse_inverted_switch(self):
+        self.machine.default_platform.write_hw_rule = MagicMock()
+        self.machine.autofires.ac_test_inverted.enable()
+
+        self.assertEqual(
+            (self.machine.switches.s_test_nc,
+             0,
+             self.machine.coils.c_test2,
+             'pulse',
+             False,
+             False), self.machine.default_platform.write_hw_rule._mock_call_args_list[0][0])
+
+        self.machine.default_platform.clear_hw_rule = MagicMock()
+        self.machine.autofires.ac_test_inverted.disable()
+
+        self.machine.default_platform.clear_hw_rule.assert_called_once_with("s_test_nc")
