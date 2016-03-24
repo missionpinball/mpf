316a53249a8da17097e54ffcc4fe983e1106eb85
Jan Kantert <jan-mpf@kantert.net>
2016-03-24 01:24:15 +0100

    enable all plugins and increase duration

--- mpf/tests/MpfMachineTestCase.py
+++ mpf/tests/MpfMachineTestCase.py
@@ -3,6 +3,16 @@ from mpf.tests.MpfTestCase import MpfTestCase
 
 class MpfMachineTestCase(MpfTestCase):
 
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName)
+
+        # only disable bcp. everything else should run
+        self.machine_config_patches = dict()
+        self.machine_config_patches['bcp'] = []
+
+        # increase test expected duration
+        self.expected_duration = 5.0
+
     def getConfigFile(self):
         return "config.yaml"
 
