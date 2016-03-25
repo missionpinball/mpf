07fcd4816567026e22b05f37039c70361562340c
Jan Kantert <jan-mpf@kantert.net>
2016-03-25 14:19:50 +0100

    add test for BallController bug

--- mpf/tests/test_BallController.py
+++ mpf/tests/test_BallController.py
@@ -1,9 +1,5 @@
-import unittest
-
-from mpf.core.machine import MachineController
 from mpf.tests.MpfTestCase import MpfTestCase
 from mock import MagicMock
-import time
 
 
 class TestBallController(MpfTestCase):
@@ -66,3 +62,32 @@ class TestBallController(MpfTestCase):
         self.machine.switch_controller.process_switch("s_start", 0)
         self.advance_time_and_run(1)
         self.assertNotEqual(None, self.machine.game)
+
+    def test_ball_collect(self):
+        self.machine.switch_controller.process_switch("s_ball_switch1", 1)
+        self.machine.switch_controller.process_switch("s_ball_switch3", 1)
+        self.machine.switch_controller.process_switch("s_ball_switch4", 1)
+        self.advance_time_and_run(1)
+        self.assertEqual(3, self.machine.ball_controller.num_balls_known)
+
+        # prevent smart_virtual from "ejecting" the ball
+        self.machine.coils.eject_coil2.pulse = MagicMock()
+
+        self.machine.switch_controller.process_switch("s_ball_switch_launcher",
+                                                      1)
+        self.advance_time_and_run(.5)
+        self.assertEqual(4, self.machine.ball_controller.num_balls_known)
+
+        self.assertEqual("idle", self.machine.ball_devices.test_trough._state)
+        self.assertEqual("ejecting", self.machine.ball_devices.test_launcher._state)
+
+        # not all balls are home. it should trigger a collect
+        self.machine.switch_controller.process_switch("s_start", 1)
+        self.machine.switch_controller.process_switch("s_start", 0)
+        self.advance_time_and_run(.1)
+        self.assertEqual(None, self.machine.game)
+
+        # ball is still in launcher and the collect ball should not try to eject it again (which would request a ball
+        # at the trough. so check that trough is idle
+        self.assertEqual("idle", self.machine.ball_devices.test_trough._state)
+        self.assertEqual("ejecting", self.machine.ball_devices.test_launcher._state)
