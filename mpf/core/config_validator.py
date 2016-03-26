8a08f7e13430319f5d298eea0462c43b451797e4
Brian Madden <brian@missionpinball.com>
2016-03-26 08:47:51 -0700

    Remove long_pulse_ms from validator

--- mpf/core/config_validator.py
+++ mpf/core/config_validator.py
@@ -141,7 +141,6 @@ coils:
     number: single|str|
     number_str: single|str|
     pulse_ms: single|int|None
-    long_pulse_ms: single|int|None
     pwm_on_ms: single|int|None
     pwm_off_ms: single|int|None
     pulse_power: single|int|None
