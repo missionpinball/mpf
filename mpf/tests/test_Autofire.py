06309a385e31f9d8f4e62ff140f53b63ce014ea4
Jan Kantert <jan-mpf@kantert.net>
2016-03-28 17:59:32 +0200

    add test for autofire reconfigured switch

--- mpf/tests/test_Autofire.py
+++ mpf/tests/test_Autofire.py
@@ -14,7 +14,7 @@ class TestAutofire(MpfTestCase):
         self.machine.autofires.ac_test.enable()
 
         self.assertEqual(
-            (self.machine.switches.s_test,
+            (self.machine.autofires.ac_test.switch,
              1,
              self.machine.coils.c_test,
              'pulse',
@@ -31,7 +31,24 @@ class TestAutofire(MpfTestCase):
         self.machine.autofires.ac_test_inverted.enable()
 
         self.assertEqual(
-            (self.machine.switches.s_test_nc,
+            (self.machine.autofires.ac_test_inverted.switch,
+             0,
+             self.machine.coils.c_test2,
+             'pulse',
+             False,
+             False), self.machine.default_platform.write_hw_rule._mock_call_args_list[0][0])
+
+        self.machine.default_platform.clear_hw_rule = MagicMock()
+        self.machine.autofires.ac_test_inverted.disable()
+
+        self.machine.default_platform.clear_hw_rule.assert_called_once_with("s_test_nc")
+
+    def test_hw_rule_pulse_inverted_autofire(self):
+        self.machine.default_platform.write_hw_rule = MagicMock()
+        self.machine.autofires.ac_test_inverted2.enable()
+
+        self.assertEqual(
+            (self.machine.autofires.ac_test_inverted2.switch,
              0,
              self.machine.coils.c_test2,
              'pulse',
