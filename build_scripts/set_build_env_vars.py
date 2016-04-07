06fe57891c95053626025db8ba3016e6d6f197d4
Brian Madden <brian@missionpinball.com>
2016-04-06 22:46:39 -0700

    updated appveyor config

--- build_scripts/set_build_env_vars.py
+++ build_scripts/set_build_env_vars.py
@@ -3,28 +3,29 @@ import git
 mpf_repo = git.Repo('c:\\projects\\mpf')
 mpf_git = git.Git('c:\\projects\\mpf')
 
+
 # http://stackoverflow.com/questions/25556696/python-get-a-list-of-changed-files-between-two-commits-or-branches
 def git_diff(branch1, branch2):
-    format = '--name-only'
+    fmt = '--name-only'
     commits = list()
-    differ = mpf_git.diff('%s..%s' % (branch1, branch2), format).split("\n")
+    differ = mpf_git.diff('%s..%s' % (branch1, branch2), fmt).split("\n")
     for line in differ:
         if len(line):
             commits.append(line)
 
     return commits
 
-commits = list(mpf_repo.iter_commits('dev', max_count=2))
+commit_list = list(mpf_repo.iter_commits('dev', max_count=2))
 
-if 'mpf/_version.py' in git_diff(commits[0], commits[1]):
-    print("Setting DEPLOY_TO_PYPI = 1")
-    deploy = 1
-else:
-    print("Setting DEPLOY_TO_PYPI = 0")
-    deploy = 0
+deploy = True
+if 'mpf/_version.py' not in git_diff(commit_list[0], commit_list[1]):
+    deploy = False
 
 # Environment variables are only available during this Python process, so write
 # the result to a batch file which will be run in the next step to set it for
 # real.
 with open('set_env.bat', 'w') as f:
-    f.write('set DEPLOY_TO_PYPI={}'.format(deploy))
+    if not deploy:
+        f.write('set DEPLOY_TO_PYPI=0')
+    else:
+        pass
