ea4546cf6c29674f40e483ddd8002dc6fff17e72
Jan Kantert <jan-mpf@kantert.net>
2016-04-22 19:08:31 +0200

    set num_balls_known

--- mpf/tests/test_PlayfieldTransfer.py
+++ mpf/tests/test_PlayfieldTransfer.py
@@ -17,6 +17,7 @@ class TestPlayfieldTransfer(MpfTestCase):
         pf1 = self.machine.ball_devices['playfield1']
         pf2 = self.machine.ball_devices['playfield2']
 
+        self.machine.ball_controller.num_balls_known = 4
         pf1.balls = 2
         pf1.available_balls = 2
         pf2.balls = 2
