c4b27855fbf47f3d1f31c73eb3a00da5456b95ec
Jan Kantert <jan-mpf@kantert.net>
2016-04-28 22:06:52 +0200

    add correction logic to BallController

--- mpf/core/ball_controller.py
+++ mpf/core/ball_controller.py
@@ -48,9 +48,88 @@ class BallController(object):
                 self.machine.switch_controller.add_switch_handler(switch.name,
                                                                   self._update_num_balls_known,
                                                                   ms=device.config['entrance_count_delay'])
+                self.machine.switch_controller.add_switch_handler(switch.name,
+                                                                  self._correct_playfield_count,
+                                                                  ms=device.config['entrance_count_delay'],
+                                                                  state=1)
+                self.machine.switch_controller.add_switch_handler(switch.name,
+                                                                  self._correct_playfield_count,
+                                                                  ms=device.config['exit_count_delay'],
+                                                                  state=0)
+
+        for playfield in self.machine.playfields:
+            self.machine.events.add_handler('{}_ball_count_change'.format(playfield.name),
+                                            self._correct_playfield_count)
 
         # run initial count
         self._update_num_balls_known()
+
+    def _get_loose_balls(self):
+        return self.num_balls_known - self._count_stable_balls()
+
+    def _count_stable_balls(self):
+        self.log.debug("Counting Balls")
+        balls = 0
+
+        for device in self.machine.ball_devices:
+            if device.is_playfield():
+                continue
+
+            if not device.is_ball_count_stable():
+                raise ValueError("devices not stable")
+
+            # generally we do not count ball devices without switches
+            if 'ball_switches' not in device.config:
+                continue
+            # special handling for troughs (needed for gottlieb)
+            elif not device.config['ball_switches'] and 'trough' in device.tags:
+                balls += device.balls
+            else:
+                for switch in device.config['ball_switches']:
+                    if self.machine.switch_controller.is_active(
+                            switch.name, ms=device.config['entrance_count_delay']):
+                        balls += 1
+                    elif self.machine.switch_controller.is_inactive(
+                            switch.name, ms=device.config['exit_count_delay']):
+                        continue
+                    else:
+                        raise ValueError("switches not stable")
+
+        return balls
+
+    def _correct_playfield_count(self, **kwargs):
+        del kwargs
+        self.delay.reset(ms=1, callback=self._correct_playfield_count2, name="correct_playfield")
+
+    def _correct_playfield_count2(self):
+        try:
+            loose_balls = self._get_loose_balls()
+        except ValueError:
+            self.delay.reset(ms=10000, callback=self._correct_playfield_count2, name="correct_playfield")
+            return
+
+        balls_on_pfs = 0
+
+        for playfield in self.machine.playfields:
+            balls_on_pfs += playfield.balls
+
+        # fix too much balls
+        while balls_on_pfs > loose_balls:
+            for playfield in self.machine.playfields:
+                self.log.warning("Corecting balls on pf from %s to %s",
+                                 playfield.balls, playfield.balls - 1)
+                if playfield.balls > 0:
+                    if playfield.unexpected_balls > 0:
+                        playfield.unexpected_balls -= 1
+                    playfield.balls -= 1
+                    balls_on_pfs -= 1
+                    break
+
+        for playfield in self.machine.playfields:
+            if playfield.balls != playfield.available_balls:
+                self.log.warning("Corecting available_balls %s to %s",
+                                 playfield.available_balls, playfield.balls)
+                playfield.available_balls = playfield.balls
 
     def _update_num_balls_known(self):
         balls = self._count_balls()
@@ -174,9 +253,6 @@ class BallController(object):
                            "Total balls known: %s", count,
                            self.machine.ball_controller.num_balls_known)
             return False
-
-    def get_free_balls(self):
-        return self.num_balls_known - self._count_balls()
 
     def collect_balls(self, target='home, trough'):
         """Used to ensure that all balls are in contained in ball devices with
