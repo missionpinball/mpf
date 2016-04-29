567318efbad564f423d5a930b1a83dc438e0d5f9
Jan Kantert <jan-mpf@kantert.net>
2016-04-29 18:41:14 +0200

    remove tests of glass_of_mode

--- mpf/tests/test_Playfield.py
+++ mpf/tests/test_Playfield.py
@@ -9,28 +9,4 @@ class TestPlayfield(MpfTestCase):
     def getMachinePath(self):
         return 'tests/machine_files/playfield/'
 
-    def test_unexpected_ball_on_pf(self):
-        self.set_num_balls_known(1)
-        self.assertEqual(0, self.machine.ball_devices.playfield.balls)
-        self.assertEqual(False, self.machine.config['machine']['glass_off_mode'])
-
-        self.machine.switch_controller.process_switch("s_playfield", 1)
-        self.machine.switch_controller.process_switch("s_playfield", 0)
-        self.advance_time_and_run(.1)
-
-        self.assertEqual(1, self.machine.ball_devices.playfield.balls)
-        self.assertEqual(1, self.machine.ball_devices.playfield.available_balls)
-
-    def test_unexpected_ball_on_pf_glass_off_mode(self):
-        self.set_num_balls_known(1)
-        self.machine.config['machine']['glass_off_mode'] = True
-
-        self.assertEqual(0, self.machine.ball_devices.playfield.balls)
-        self.assertEqual(True, self.machine.config['machine']['glass_off_mode'])
-
-        self.machine.switch_controller.process_switch("s_playfield", 1)
-        self.machine.switch_controller.process_switch("s_playfield", 0)
-        self.advance_time_and_run(.1)
-
-        self.assertEqual(0, self.machine.ball_devices.playfield.balls)
-        self.assertEqual(0, self.machine.ball_devices.playfield.available_balls)
+    # nothing to test currently
