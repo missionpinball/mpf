bb3822e3df728fb47d091415e88e28debcc164a6
Jan Kantert <jan-mpf@kantert.net>
2016-05-06 14:12:41 +0200

    complete SerialCommunicator test

--- mpf/tests/test_FastSerial.py
+++ mpf/tests/test_FastSerial.py
@@ -1,6 +1,9 @@
 import unittest
 from queue import Queue
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
+
+import time
+
 from mpf.platforms import fast
 
 
@@ -8,10 +11,7 @@ class SerialMock:
 
     def read(self, length):
         del length
-        if not self.send_buffer:
-            msg = self.queue.get()
-        else:
-            msg = self.send_buffer.pop()
+        msg = self.queue.get()
         return msg
 
     def __getattr__(self, item):
@@ -48,7 +48,7 @@ class SerialMock:
 
     def write(self, msg):
         if msg in self.permanent_commands:
-            self.send_buffer.append(self.permanent_commands[msg])
+            self.queue.put(self.permanent_commands[msg])
             return
 
         # print("Serial received: " + "".join("\\x%02x" % ord(b) for b in msg) + " len: " + str(len(msg)))
@@ -58,13 +58,12 @@ class SerialMock:
                                  " len: " + str(len(msg)))
 
         if self.expected_commands[msg] is not False:
-            self.send_buffer.append(self.expected_commands[msg])
+            self.queue.put(self.expected_commands[msg])
 
         del self.expected_commands[msg]
 
     def __init__(self):
         self.name = "SerialMock"
-        self.send_buffer = []
         self.expected_commands = {}
         self.queue = Queue()
         self.permanent_commands = {}
@@ -74,18 +73,20 @@ class SerialMock:
 
 class TestFastSerial(unittest.TestCase):
 
-    def test_SerialCommunicator_DMD(self):
-
+    def setUp(self):
         self.machine = MagicMock()
         self.platform = MagicMock()
+        self.platform.config['debug'] = True
+        self.platform.machine_type = "fast"
         self.send_queue = Queue()
         self.receive_queue = Queue()
 
         self.serialMock = SerialMock()
         fast.serial.Serial = MagicMock(return_value=self.serialMock)
 
+    def test_SerialCommunicator_DMD(self):
         self.serialMock.expected_commands = {
-            'ID:\r'.encode(): 'ID:DMD FP-CPU-002-1 00.88\r\n'.encode()
+            'ID:\r'.encode(): 'ID:DMD FP-CPU-002-1 00.88\r'.encode()
         }
 
         self.communicator = fast.SerialCommunicator(
@@ -94,18 +95,23 @@ class TestFastSerial(unittest.TestCase):
             receive_queue=self.receive_queue)
 
         self.platform.register_processor_connection.assert_called_with('DMD', self.communicator)
+        self.assertFalse(self.serialMock.expected_commands)
+
+        self.communicator.send("test".encode())
+        self.serialMock.expected_commands = {
+            'BM:test'.encode(): False
+        }
+        time.sleep(.001)
+        for dummy_i in range(10):
+            if not self.serialMock.expected_commands:
+                break
+            time.sleep(.1)
+
+        self.assertFalse(self.serialMock.expected_commands)
 
     def test_SerialCommunicator_RGB(self):
-        self.machine = MagicMock()
-        self.platform = MagicMock()
-        self.send_queue = Queue()
-        self.receive_queue = Queue()
-
-        self.serialMock = SerialMock()
-        fast.serial.Serial = MagicMock(return_value=self.serialMock)
-
         self.serialMock.expected_commands = {
-            'ID:\r'.encode(): 'ID:RGB FP-CPU-002-1 00.88\r\n'.encode()
+            'ID:\r'.encode(): 'ID:RGB FP-CPU-002-1 00.88\r'.encode()
         }
 
         self.communicator = fast.SerialCommunicator(
@@ -114,21 +120,13 @@ class TestFastSerial(unittest.TestCase):
             receive_queue=self.receive_queue)
 
         self.platform.register_processor_connection.assert_called_with('RGB', self.communicator)
+        self.assertFalse(self.serialMock.expected_commands)
 
     def test_SerialCommunicator_NET(self):
-        self.machine = MagicMock()
-        self.platform = MagicMock()
-        self.platform.machine_type = "fast"
-        self.send_queue = Queue()
-        self.receive_queue = Queue()
-
-        self.serialMock = SerialMock()
-        fast.serial.Serial = MagicMock(return_value=self.serialMock)
-
         self.serialMock.expected_commands = {
             'ID:\r'.encode(): 'ID:NET FP-CPU-002-1 00.88\r'.encode(),
             'NN:0\r'.encode(): 'NN:0,asd,0.87,8,10,,,,,,\r'.encode(),
-            'NN:1\r'.encode(): 'NN:1,asd,0.87,8,10,,,,,,\r\r'.encode(),
+            'NN:1\r'.encode(): 'NN:1,asd,0.87,8,10,,,,,,\r'.encode(),
             'NN:2\r'.encode(): 'NN:2,asd,0.87,8,10,,,,,,\r'.encode(),
             'NN:3\r'.encode(): 'NN:3,asd,0.87,8,10,,,,,,\r'.encode(),
             'NN:4\r'.encode(): 'NN:4,asd,0.87,8,10,,,,,,\r'.encode(),
@@ -142,4 +140,32 @@ class TestFastSerial(unittest.TestCase):
             baud=1234, send_queue=Queue(),
             receive_queue=self.receive_queue)
 
+        self.assertFalse(self.serialMock.expected_commands)
+
         self.platform.register_processor_connection.assert_called_with('NET', self.communicator)
+        self.platform.log.info.assert_has_calls([
+            call('Connecting to %s at %sbps', 'port_name', 1234),
+            call('Received ID acknowledgement. Processor: %s, Board: %s, Firmware: %s', 'NET', 'FP-CPU-002-1', '00.88'),
+            call('Fast IO Board 0: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 1: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 2: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 3: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 4: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 5: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 6: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8'),
+            call('Fast IO Board 7: Model: asd, Firmware: 0.87, Switches: 16, Drivers: 8')
+        ])
+
+        self.communicator.send("SA:")
+        self.serialMock.expected_commands = {
+            "SA:\r".encode(): "SA:1,00,8,00000000\r".encode()
+        }
+        time.sleep(.001)
+        for dummy_i in range(10):
+            if not self.serialMock.expected_commands and not self.communicator.receive_queue.empty():
+                break
+            time.sleep(.1)
+
+        self.assertFalse(self.serialMock.expected_commands)
+        self.assertFalse(self.communicator.receive_queue.empty())
+        self.assertEqual("SA:1,00,8,00000000", self.communicator.receive_queue.get())
