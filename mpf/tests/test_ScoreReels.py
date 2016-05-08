b221837b83e3e49102e13483012de88738ba6464
Jan Kantert <jan-mpf@kantert.net>
2016-05-08 19:01:45 +0200

    test failure scenario

--- mpf/tests/test_ScoreReels.py
+++ mpf/tests/test_ScoreReels.py
@@ -124,3 +124,49 @@ class TestShots(MpfTestCase):
         self.assertEqual(1, player1_1k.pulse.call_count)
         self.assertEqual(1, player1_100.pulse.call_count)
         self.assertEqual(9, player1_10.pulse.call_count)
+
+    def testAdvanceingFailure(self):
+        player1_10k = self.machine.coils.player1_10k.hw_driver
+        player1_1k = self.machine.coils.player1_1k.hw_driver
+        player1_100 = self.machine.coils.player1_100.hw_driver
+        player1_10 = self.machine.coils.player1_10.hw_driver
+        player1_10k.pulse = MagicMock(return_value=10)
+        player1_1k.pulse = MagicMock(return_value=10)
+        player1_100.pulse = MagicMock(return_value=10)
+        player1_10.pulse = MagicMock(return_value=10)
+        self.start_game()
+
+        self.machine.scoring.add(110)
+        self.advance_time_and_run(.1)
+        self.assertEqual(0, player1_10k.pulse.call_count)
+        self.assertEqual(0, player1_1k.pulse.call_count)
+        self.assertEqual(1, player1_100.pulse.call_count)
+        self.assertEqual(1, player1_10.pulse.call_count)
+        self.release_switch_and_run("score_1p_10_0", 0)
+        # switch for pos 0 stays on
+
+        # it retries
+        self.advance_time_and_run(.3)
+        self.machine_run()
+        self.assertEqual(0, player1_10k.pulse.call_count)
+        self.assertEqual(0, player1_1k.pulse.call_count)
+        self.assertEqual(2, player1_100.pulse.call_count)
+        self.assertEqual(1, player1_10.pulse.call_count)
+
+        # and again
+        self.advance_time_and_run(.3)
+        self.machine_run()
+        self.assertEqual(0, player1_10k.pulse.call_count)
+        self.assertEqual(0, player1_1k.pulse.call_count)
+        self.assertEqual(3, player1_100.pulse.call_count)
+        self.assertEqual(1, player1_10.pulse.call_count)
+        # finally
+        self.release_switch_and_run("score_1p_100_0", 0)
+
+        # no more retries
+        self.advance_time_and_run(10)
+        self.machine_run()
+        self.assertEqual(0, player1_10k.pulse.call_count)
+        self.assertEqual(0, player1_1k.pulse.call_count)
+        self.assertEqual(3, player1_100.pulse.call_count)
+        self.assertEqual(1, player1_10.pulse.call_count)
