c7132ef7e16ac0c53e6e653706cfee3d4227f9e7
Jan Kantert <jan-mpf@kantert.net>
2016-05-11 02:13:16 +0200

    test drop targets and drop target banks

--- mpf/tests/test_DropTargets.py
+++ mpf/tests/test_DropTargets.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 from mpf.tests.MpfTestCase import MpfTestCase
 
 
@@ -9,11 +11,49 @@ class TestDropTargets(MpfTestCase):
     def getMachinePath(self):
         return 'tests/machine_files/drop_targets/'
 
-    def test_drop_targets(self):
+    def test_drop_target_bank(self):
         self.assertIn('left1', self.machine.drop_targets)
         self.assertIn('left2', self.machine.drop_targets)
         self.assertIn('left3', self.machine.drop_targets)
         self.assertIn('left_bank', self.machine.drop_target_banks)
+
+        self.machine.coils.coil1.pulse = MagicMock()
+
+        self.assertFalse(self.machine.drop_targets.left1.complete)
+        self.assertFalse(self.machine.drop_targets.left2.complete)
+        self.assertFalse(self.machine.drop_targets.left3.complete)
+        self.assertFalse(self.machine.drop_target_banks.left_bank.complete)
+
+        self.hit_switch_and_run("switch1", 1)
+        self.hit_switch_and_run("switch2", 1)
+        self.assertTrue(self.machine.drop_targets.left1.complete)
+        self.assertTrue(self.machine.drop_targets.left2.complete)
+        self.assertFalse(self.machine.drop_targets.left3.complete)
+        self.assertFalse(self.machine.drop_target_banks.left_bank.complete)
+
+        assert not self.machine.coils.coil1.pulse.called
+
+        self.hit_switch_and_run("switch3", .5)
+        self.assertTrue(self.machine.drop_targets.left1.complete)
+        self.assertTrue(self.machine.drop_targets.left2.complete)
+        self.assertTrue(self.machine.drop_targets.left3.complete)
+        self.assertTrue(self.machine.drop_target_banks.left_bank.complete)
+
+        assert not self.machine.coils.coil1.pulse.called
+
+        # it should reset after 1s
+        self.advance_time_and_run(.5)
+        self.machine.coils.coil1.pulse.assert_called_once_with()
+
+        # after another 100ms the switches relesae
+        self.release_switch_and_run("switch1", 0)
+        self.release_switch_and_run("switch2", 0)
+        self.release_switch_and_run("switch3", 1)
+
+        self.assertFalse(self.machine.drop_targets.left1.complete)
+        self.assertFalse(self.machine.drop_targets.left2.complete)
+        self.assertFalse(self.machine.drop_targets.left3.complete)
+        self.assertFalse(self.machine.drop_target_banks.left_bank.complete)
 
     def test_drop_targets_in_mode(self):
         self.machine.modes['mode1'].start()
