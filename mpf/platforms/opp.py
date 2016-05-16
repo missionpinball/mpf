f6d8ddbedeb7c8a42d823c72bb61d99946173819
Jan Kantert <jan-mpf@kantert.net>
2016-05-16 10:37:36 +0200

    move switch out of opp

--- mpf/platforms/opp.py
+++ mpf/platforms/opp.py
@@ -11,6 +11,7 @@ import queue
 import traceback
 
 from mpf.platforms.opp_common.opp_neopixel import OPPNeopixelCard
+from mpf.platforms.opp_common.opp_switch import OPPInputCard
 
 try:
     import serial
@@ -707,29 +708,6 @@ class OPPSolenoidCard(object):
         return_dict['pulse_ms'] = str(pulse_ms)
         return_dict['hold_power'] = '0'
         return return_dict
-
-
-class OPPInputCard(object):
-    def __init__(self, addr, mask, inp_dict, inp_addr_dict):
-        self.log = logging.getLogger('OPPInputCard')
-        self.addr = addr
-        self.oldState = 0
-        self.mask = mask
-        self.cardNum = str(addr - ord(OppRs232Intf.CARD_ID_GEN2_CARD))
-
-        self.log.debug("Creating OPP Input at hardware address: 0x%02x", addr)
-
-        inp_addr_dict[addr] = self
-        for index in range(0, 32):
-            if ((1 << index) & mask) != 0:
-                inp_dict[self.cardNum + '-' + str(index)] = OPPSwitch(self, self.cardNum + '-' + str(index))
-
-
-class OPPSwitch(object):
-    def __init__(self, card, number):
-        self.number = number
-        self.card = card
-        self.config = {}
 
 
 class SerialCommunicator(object):
---
+++ mpf/platforms/opp_common/opp_switch.py
@@ -0,0 +1,26 @@
+import logging
+
+from mpf.platforms.opp_common.opp_rs232_intf import OppRs232Intf
+
+
+class OPPInputCard(object):
+    def __init__(self, addr, mask, inp_dict, inp_addr_dict):
+        self.log = logging.getLogger('OPPInputCard')
+        self.addr = addr
+        self.oldState = 0
+        self.mask = mask
+        self.cardNum = str(addr - ord(OppRs232Intf.CARD_ID_GEN2_CARD))
+
+        self.log.debug("Creating OPP Input at hardware address: 0x%02x", addr)
+
+        inp_addr_dict[addr] = self
+        for index in range(0, 32):
+            if ((1 << index) & mask) != 0:
+                inp_dict[self.cardNum + '-' + str(index)] = OPPSwitch(self, self.cardNum + '-' + str(index))
+
+
+class OPPSwitch(object):
+    def __init__(self, card, number):
+        self.number = number
+        self.card = card
+        self.config = {}
