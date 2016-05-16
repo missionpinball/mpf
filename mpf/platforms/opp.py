c27cfc679578a0ae8a64a069881b482f133e4f44
Jan Kantert <jan-mpf@kantert.net>
2016-05-16 10:40:34 +0200

    move incand out of opp

--- mpf/platforms/opp.py
+++ mpf/platforms/opp.py
@@ -11,6 +11,7 @@ import queue
 import traceback
 
 from mpf.platforms.opp_common.opp_coil import OPPSolenoidCard
+from mpf.platforms.opp_common.opp_incand import OPPIncandCard
 from mpf.platforms.opp_common.opp_neopixel import OPPNeopixelCard
 from mpf.platforms.opp_common.opp_switch import OPPInputCard
 
@@ -566,48 +567,6 @@ class HardwarePlatform(MatrixLightsPlatform, LedPlatform, SwitchPlatform, Driver
 
         coil.hw_driver.use_switch = False
         self.reconfigure_driver(coil, not coil.hw_driver.can_be_pulsed)
-
-
-class OPPIncandCard(object):
-
-    def __init__(self, addr, mask, incand_dict):
-        self.log = logging.getLogger('OPPIncand')
-        self.addr = addr
-        self.oldState = 0
-        self.newState = 0
-        self.mask = mask
-
-        self.log.debug("Creating OPP Incand at hardware address: 0x%02x", addr)
-
-        card = str(addr - ord(OppRs232Intf.CARD_ID_GEN2_CARD))
-        for index in range(0, 32):
-            if ((1 << index) & mask) != 0:
-                number = card + '-' + str(index)
-                incand_dict[number] = OPPIncand(self, number)
-
-
-class OPPIncand(object):
-
-    def __init__(self, incand_card, number):
-        self.incandCard = incand_card
-        self.number = number
-
-    def off(self):
-        """Disables (turns off) this matrix light."""
-        _, incand = self.number.split("-")
-        curr_bit = (1 << int(incand))
-        self.incandCard.newState &= ~curr_bit
-
-    def on(self, brightness=255, fade_ms=0, start=0):
-        """Enables (turns on) this driver."""
-        del fade_ms
-        del start
-        _, incand = self.number.split("-")
-        curr_bit = (1 << int(incand))
-        if brightness == 0:
-            self.incandCard.newState &= ~curr_bit
-        else:
-            self.incandCard.newState |= curr_bit
 
 
 class SerialCommunicator(object):
---
+++ mpf/platforms/opp_common/opp_incand.py
@@ -0,0 +1,45 @@
+import logging
+
+from mpf.platforms.opp_common.opp_rs232_intf import OppRs232Intf
+
+
+class OPPIncandCard(object):
+
+    def __init__(self, addr, mask, incand_dict):
+        self.log = logging.getLogger('OPPIncand')
+        self.addr = addr
+        self.oldState = 0
+        self.newState = 0
+        self.mask = mask
+
+        self.log.debug("Creating OPP Incand at hardware address: 0x%02x", addr)
+
+        card = str(addr - ord(OppRs232Intf.CARD_ID_GEN2_CARD))
+        for index in range(0, 32):
+            if ((1 << index) & mask) != 0:
+                number = card + '-' + str(index)
+                incand_dict[number] = OPPIncand(self, number)
+
+
+class OPPIncand(object):
+
+    def __init__(self, incand_card, number):
+        self.incandCard = incand_card
+        self.number = number
+
+    def off(self):
+        """Disables (turns off) this matrix light."""
+        _, incand = self.number.split("-")
+        curr_bit = (1 << int(incand))
+        self.incandCard.newState &= ~curr_bit
+
+    def on(self, brightness=255, fade_ms=0, start=0):
+        """Enables (turns on) this driver."""
+        del fade_ms
+        del start
+        _, incand = self.number.split("-")
+        curr_bit = (1 << int(incand))
+        if brightness == 0:
+            self.incandCard.newState &= ~curr_bit
+        else:
+            self.incandCard.newState |= curr_bit
