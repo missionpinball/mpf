27f28cfd77f9c2fa2e4b025c3bdd2efdde9d1412
Jan Kantert <jan-mpf@kantert.net>
2016-05-22 14:20:51 +0200

    turn on documentation warnings

--- mpf/plugins/__init__.py
+++ mpf/plugins/__init__.py
@@ -1,7 +1,1 @@
-# import all the modules in this folder that do not start with an underscore
 
-import os
-import glob
-_modules = glob.glob(os.path.dirname(__file__) + "/*.py")
-__all__ = (os.path.basename(f)[:-3] for f in _modules
-           if not os.path.basename(f).startswith('_'))
