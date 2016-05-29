a455547b1b502a75ac81f8d130a2211ead3377ff
Jan Kantert <jan-mpf@kantert.net>
2016-05-29 11:28:01 +0200

    timings are slightly different in tests

--- mpf/tests/test_ScoreReels.py
+++ mpf/tests/test_ScoreReels.py
@@ -60,7 +60,6 @@ class TestShots(MpfTestCase):
 
         self.machine.scoring.add(11097)  # result: 11207
         self.advance_time_and_run(.1)
-        self.advance_time_and_run(.1)
         self.assertEqual(0, player1_10k.pulse.call_count)
         self.assertEqual(0, player1_1k.pulse.call_count)
         self.assertEqual(0, player1_100.pulse.call_count)
@@ -91,7 +90,6 @@ class TestShots(MpfTestCase):
         self.assertEqual(7, player1_10.pulse.call_count)
 
         self.advance_time_and_run(.3)
-        self.machine_run()
         self.assertEqual(0, player1_10k.pulse.call_count)
         self.assertEqual(0, player1_1k.pulse.call_count)
         self.assertEqual(0, player1_100.pulse.call_count)
@@ -240,7 +238,7 @@ class TestShots(MpfTestCase):
 
         # drain ball
         self.machine.game.balls_in_play = 0
-        self.machine_run()
+        self.advance_time_and_run(.1)
         self.assertEqual(3, self.machine.game.player.number)
 
         # player3 reuses the reels from player 1. machine resets them
