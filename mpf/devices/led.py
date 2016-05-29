775928a69e6b6f55f436049a477462be06e58e37
Jan Kantert <jan-mpf@kantert.net>
2016-05-29 18:48:10 +0200

    make led update speed configurable

--- mpf/devices/led.py
+++ mpf/devices/led.py
@@ -48,7 +48,9 @@ class Led(SystemWideDevice):
         # schedule the single machine-wide update to write the current led of
         # each LED to the hardware
         # todo make time configurable
-        machine.clock.schedule_interval(cls.update_leds, 0, -100)
+        machine.clock.schedule_interval(cls.update_leds,
+                                        1 / machine.config['mpf']['default_led_hw_update_hz'],
+                                        -100)
 
         machine.mode_controller.register_stop_method(cls.mode_stop)
 
