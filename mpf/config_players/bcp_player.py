96f924a7497967b7074c164d9cab39077ee31d08
Jan Kantert <jan-mpf@kantert.net>
2016-06-12 14:30:27 +0200

    implements stubs for bcp

--- mpf/config_players/bcp_player.py
+++ mpf/config_players/bcp_player.py
@@ -1,9 +1,18 @@
+"""Stub for BCP Player."""
 from mpf.core.config_player import ConfigPlayer
 
 
 class BcpPlayer(ConfigPlayer):
 
+    """Dummy BCP Player."""
+
     config_file_section = 'bcp_player'
     show_section = 'bcp'
+
+    def play(self, settings, context, priority=0, **kwargs):
+        pass
+
+    def get_express_config(self, value):
+        pass
 
 player_cls = BcpPlayer
