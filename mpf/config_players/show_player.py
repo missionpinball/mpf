edd522056c3fdf46c1039f0c61f4216521193d62
Jan Kantert <jan-mpf@kantert.net>
2016-06-12 14:32:43 +0200

    remember shows in context and stop them at the end

--- mpf/config_players/show_player.py
+++ mpf/config_players/show_player.py
@@ -8,9 +8,7 @@ class ShowPlayer(ConfigPlayer):
     show_section = 'shows'
     device_collection = None
 
-    def play(self, settings, key=None, priority=0, **kwargs):
-        # todo should show_tokens be part of settings?
-
+    def play(self, settings, context, priority=0, **kwargs):
         if 'shows' in settings:
             settings = settings['shows']
 
@@ -19,24 +17,24 @@ class ShowPlayer(ConfigPlayer):
         # show_tokens = kwargs.get('show_tokens', None)
 
         for show, s in settings.items():
-            if 'hold' in s:
+            if 'hold' in s and s['hold'] is not None:
                 raise AssertionError("Setting 'hold' is no longer supported for shows. Use duration -1 in your show.")
             try:
                 s['priority'] += priority
             except KeyError:
                 s['priority'] = priority
 
-            if not s['key']:
-                s['key'] = key
-
             # todo need to add this key back to the config player
 
-            self._update_show(show, s)
+            self._update_show(show, s, context)
 
-    def _update_show(self, show, s):
+    def _update_show(self, show, s, context):
+        instance_dict = self._get_instance_dict(context)
         if s['action'].lower() == 'play':
+            if show in instance_dict:
+                instance_dict[show].stop()
             try:
-                self.machine.shows[show].play(
+                show_instance = self.machine.shows[show].play(
                     show_tokens=s['show_tokens'],
                     priority=s['priority'],
                     speed=s['speed'],
@@ -45,40 +43,38 @@ class ShowPlayer(ConfigPlayer):
                     sync_ms=s['sync_ms'],
                     reset=s['reset'],
                     manual_advance=s['manual_advance'],
-                    key=s['key']
                 )
+                instance_dict[show] = show_instance
             except KeyError:
                 raise KeyError("Cannot play show '{}'. No show with that "
                                "name.".format(show))
 
         elif s['action'].lower() == 'stop':
-            for running_show in (
-                    self.machine.show_controller.get_running_shows(show)):
-                running_show.stop()
+            if show in instance_dict:
+                instance_dict[self.config_file_section][show].stop()
+                del instance_dict[self.config_file_section][show]
 
         elif s['action'].lower() == 'pause':
-            for running_show in (
-                    self.machine.show_controller.get_running_shows(show)):
-                running_show.pause()
+            if show in instance_dict:
+                instance_dict[show].pause()
 
         elif s['action'].lower() == 'resume':
-            for running_show in (
-                    self.machine.show_controller.get_running_shows(show)):
-                running_show.resume()
+            if show in instance_dict:
+                instance_dict[show].resume()
 
         elif s['action'].lower() == 'advance':
-            for running_show in (
-                    self.machine.show_controller.get_running_shows(show)):
-                running_show.advance()
+            if show in instance_dict:
+                instance_dict[show].advance()
 
         elif s['action'].lower() == 'update':
-            for running_show in (
-                    self.machine.show_controller.get_running_shows(show)):
-                running_show.update(show_tokens=s['show_tokens'],
-                                    priority=s['priority'])
+            if show in instance_dict:
+                instance_dict[show].update(
+                    show_tokens=s['show_tokens'],
+                    priority=s['priority'])
 
-    def clear(self, key):
-        self.machine.show_controller.stop_shows_by_key(key)
+    def clear_context(self, context):
+        for show in self._get_instance_dict(context).values():
+            show.stop()
 
     def get_express_config(self, value):
         return dict()
