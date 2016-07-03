a892b1b747b4bbd26869c48fb93677544fe8ddf1
Jan Kantert <jan-mpf@kantert.net>
2016-07-03 20:03:18 +0200

    manage handlers in transport

--- mpf/core/bcp/bcp_transport.py
+++ mpf/core/bcp/bcp_transport.py
@@ -7,6 +7,17 @@ class BcpTransportManager:
     def __init__(self, machine):
         self._machine = machine
         self._transports = []
+        self._handlers = {}
+
+    def add_handler_to_transport(self, handler, transport):
+        if handler not in self._handlers:
+            self._handlers[handler] = []
+
+        if transport not in self._handlers[handler]:
+            self._handlers[handler].append(transport)
+
+    def get_transports_for_handler(self, handler):
+        return self._handlers.get(handler, [])
 
     def register_transport(self, transport):
         self._transports.append(transport)
@@ -14,12 +25,24 @@ class BcpTransportManager:
     def unregister_transport(self, transport):
         self._transports.remove(transport)
 
+        # remove transport from all handlers
+        for handler in self._handlers:
+            self._handlers[handler].remove(transport)
+
     def get_named_client(self, client_name):
         for client in self._transports:
             if client.name == client_name:
                 return client
         else:
             return False
+
+    def send_to_clients(self, clients, bcp_command, **kwargs):
+        for client in clients:
+            self.send_to_client(client, bcp_command, **kwargs)
+
+    def send_to_clients_with_handler(self, handler, bcp_command, **kwargs):
+        clients = self.get_transports_for_handler(handler)
+        self.send_to_clients(clients, bcp_command, **kwargs)
 
     def send_to_client(self, client, bcp_command, **kwargs):
         try:
