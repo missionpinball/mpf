1c25446d39109aefddf36e813a9655999c9b53ec
Jan Kantert <jan-mpf@kantert.net>
2016-07-12 00:45:38 +0200

    simplify test

--- mpf/tests/test_Clock.py
+++ mpf/tests/test_Clock.py
@@ -34,9 +34,7 @@ class ClockTestCase(unittest.TestCase):
         self.callback_order = []
 
     def advance_time_and_run(self, delta=1.0):
-        task_with_timeout = asyncio.wait_for(asyncio.sleep(delay=delta),
-                                             timeout=delta+1)
-        asyncio.get_event_loop().run_until_complete(task_with_timeout)
+        asyncio.get_event_loop().run_until_complete(asyncio.sleep(delay=delta))
 
     def callback1(self, number, dt):
         del dt
@@ -64,21 +62,3 @@ class ClockTestCase(unittest.TestCase):
         self.clock.unschedule(cb1)
         self.advance_time_and_run(0.001)
         self.assertEqual(counter, 1)
-
-    def test_callback_order(self):
-        # Create two callbacks that should be called in the same tick, however the one
-        # added second should be called first based on the timeout value.
-        cb1 = self.clock.schedule_once(partial(self.callback1, 2), timeout=0.0002)
-        cb2 = self.clock.schedule_once(partial(self.callback1, 1), timeout=0.0001)
-        self.advance_time_and_run(0.002)
-        self.assertTrue(self.callback_order[0] == 1 and self.callback_order[1] == 2)
-        self.callback_order.clear()
-
-        # Create two callbacks with the same time and the same priority.  The first one
-        # added should be called first.
-        self.clock.schedule_once(partial(self.callback1, 1))
-        self.clock.schedule_once(partial(self.callback1, 2))
-        self.advance_time_and_run(0.001)
-        self.assertTrue(self.callback_order[0] == 1 and self.callback_order[1] == 2)
-        self.callback_order.clear()
-
