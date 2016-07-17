9786c0c148d6ef853e691f2a36645f972e915972
Jan Kantert <jan-mpf@kantert.net>
2016-07-17 14:52:01 +0200

    add docstrings

--- mpf/core/system_wide_device.py
+++ mpf/core/system_wide_device.py
@@ -1,7 +1,11 @@
+"""A system wide device which can be defined in the main config."""
 from mpf.core.device import Device
 
 
 class SystemWideDevice(Device):
+
+    """A system wide device which can be defined in the main config."""
+
     def device_added_system_wide(self):
-        # Called when a device is added system wide
+        """Called when a device is added system wide."""
         self._initialize()
