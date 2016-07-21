d4b3b7c4e3c5f3db1b92cf53f6937b2ca18cf5eb
Jan Kantert <jan-mpf@kantert.net>
2016-07-21 19:06:17 +0200

    start server

--- mpf/core/bcp/bcp.py
+++ mpf/core/bcp/bcp.py
@@ -1,4 +1,5 @@
 """BCP module."""
+from mpf.core.bcp.bcp_server import BcpServer
 from mpf.core.utility_functions import Util
 
 from mpf.core.bcp.bcp_interface import BcpInterface
@@ -17,8 +18,15 @@ class Bcp:
         self.machine.events.add_handler('init_phase_2',
                                         self._setup_bcp_connections)
 
+        self.machine.events.add_handler('init_phase_4',
+                                        self._setup_bcp_server)
+
     def send(self, bcp_command, **kwargs):
-        """Emulate legacy send."""
+        """Emulate legacy send.
+
+        Args:
+            bcp_command: Commmand to send
+        """
         self.transport.send_to_all_clients(bcp_command, **kwargs)
 
     def _setup_bcp_connections(self):
@@ -27,5 +35,11 @@ class Bcp:
             return
 
         for name, settings in self.machine.config['bcp']['connections'].items():
-            client = Util.string_to_class(settings['type'])(self.machine, name, settings, self.machine.bcp)
+            client = Util.string_to_class(settings['type'])(self.machine, name, self.machine.bcp)
+            client.connect(settings)
             self.transport.register_transport(client)
+
+    def _setup_bcp_server(self):
+        """Start an BCP server to allow other clients to connect."""
+        self.server = BcpServer(self.machine)
+        self.machine.clock.loop.run_until_complete(self.server.start())
