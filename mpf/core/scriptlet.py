e578d4846328da571ad3e8f67157858d9df3a45a
Jan Kantert <jan-mpf@kantert.net>
2016-08-07 19:01:24 +0200

    CS fixes

--- mpf/core/scriptlet.py
+++ mpf/core/scriptlet.py
@@ -5,7 +5,10 @@ from mpf.core.delays import DelayManager
 
 class Scriptlet(object):
 
+    """Baseclass for scriptlet which are simple scripts in a machine."""
+
     def __init__(self, machine, name):
+        """Initialise scriptlet."""
         self.machine = machine
         self.name = name
         self.log = logging.getLogger('Scriptlet.' + name)
@@ -17,7 +20,8 @@ class Scriptlet(object):
         return '<Scriptlet.{}>'.format(self.name)
 
     def on_load(self):
-        """Automatically called when this Scriptlet loads. It's the intention
-        that the Scriptlet writer will overwrite this method in the Scriptlet.
+        """Automatically called when this Scriptlet loads.
+
+        It's the intention that the Scriptlet writer will overwrite this method in the Scriptlet.
         """
         pass
