0014557e7125a1c1c2a8e120eef469d77d5308be
Jan Kantert <jan-mpf@kantert.net>
2016-08-19 17:54:38 +0200

    add docstrings

--- mpf/core/async_mode.py
+++ mpf/core/async_mode.py
@@ -10,16 +10,19 @@ class AsyncMode(Mode, metaclass=abc.ABCMeta):
     """Base class for asyncio modes."""
 
     def __init__(self, machine, config, name, path):
+        """Initialise async mode."""
         super().__init__(machine, config, name, path)
 
         self._task = None
 
     def _started(self):
+        """Start main task."""
         super()._started()
 
         self._task = self.machine.clock.loop.create_task(self._run())
 
     def _stopped(self):
+        """Cancel task."""
         super()._stopped()
 
         self._task.cancel()
@@ -27,4 +30,10 @@ class AsyncMode(Mode, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     @asyncio.coroutine
     def _run(self):
+        """Main task which runs as long as the mode is active.
+
+        Overwrite this function in your mode.
+
+        Its automatically canceled when the mode stops. You can catch CancelError to handle mode stop.
+        """
         pass
