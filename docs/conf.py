bcfb4ff5a372d3e8d92ae444fbbadfa3f1172186
toomanybrians <brian@missionpinball.com>
2016-08-21 20:17:43 -0700

    print path

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import mpf._version
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+print(sys.path)
 
 # -- General configuration ------------------------------------------------
 
