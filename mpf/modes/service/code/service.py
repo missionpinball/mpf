cbaa4fa2d88c6127908c18749d4b12791f2907f4
Jan Kantert <jan-mpf@kantert.net>
2016-08-24 22:51:17 +0200

    simplify service mode

--- mpf/modes/service/code/service.py
+++ mpf/modes/service/code/service.py
@@ -3,6 +3,7 @@ import asyncio
 from collections import namedtuple
 
 from mpf.core.async_mode import AsyncMode
+from mpf.core.utility_functions import Util
 
 ServiceMenuEntry = namedtuple("ServiceMenuEntry", ["label", "callback"])
 
@@ -15,63 +16,61 @@ class Service(AsyncMode):
     def get_config_spec():
         """Add validation for mode."""
         return '''
-enter_events: list|str|
-esc_events: list|str|
-up_events: list|str|
-down_events: list|str|
+door_opened_events: list|str|sw_service_door_open_active
+door_closed_events: list|str|sw_service_door_open_inactive
+high_power_on_events: list|str|sw_power_on_active
+high_power_off_events: list|str|sw_power_on_inactive
+enter_events: list|str|sw_service_enter_active
+esc_events: list|str|sw_service_esc_active
+up_events: list|str|sw_service_up_active
+down_events: list|str|sw_service_down_active
 '''
 
     def _service_mode_exit(self):
         self.machine.service.stop_service()
 
-    @asyncio.coroutine
     def _get_key(self):
-        event = yield from self.machine.events.wait_for_event_group_race({
-            'ESC': self.config['mode_settings']['esc_events'],
-            'ENTER': self.config['mode_settings']['enter_events'],
-            'UP': self.config['mode_settings']['up_events'],
-            'DOWN': self.config['mode_settings']['down_events']})
-        return event['group']
+        return Util.race({
+            self.machine.events.wait_for_any_event(self.config['mode_settings']['esc_events']): "ESC",
+            self.machine.events.wait_for_any_event(self.config['mode_settings']['enter_events']): "ENTER",
+            self.machine.events.wait_for_any_event(self.config['mode_settings']['up_events']): "UP",
+            self.machine.events.wait_for_any_event(self.config['mode_settings']['down_events']): "DOWN",
+        }, self.machine.clock.loop)
+
+    def _register_service_door_handler(self):
+        for switch in self.config['mode_settings']['door_open_switch']:
+            self.machine.switch_controller.add_switch_handler(
+                switch.name, self._service_door_handler, state=1, return_info=True)
+            self.machine.switch_controller.add_switch_handler(
+                switch.name, self._service_door_handler, state=0, return_info=True)
+
+    def _service_door_handler(self, state, **kwargs):
+        del kwargs
+        if state == 1:
+            self.machine.events.post("service_door_opened")
+        else:
+            self.machine.events.post("service_door_closed")
 
     @asyncio.coroutine
     def _run(self):
-        self.log.info("Service door opened.")
-        self.machine.events.post("service_door_opened")
+        while True:
+            # wait for key
+            key = yield from self._get_key()
 
-        try:
-            while True:
-                # wait for key
-                key = yield from self._get_key()
-
-                if key == "ENTER":
-                    # start main menu
-                    yield from self._start_main_menu()
-                elif key == "UP":
-                    # post event for mc to increase volume
-                    self.machine.events.post("master_volume_increase")
-                elif key == "DOWN":
-                    # post event for mc to decrease volume
-                    self.machine.events.post("master_volume_decrease")
-
-        except asyncio.CancelledError:
-            self.machine.events.post("service_door_closed")
-            raise
-
-    @asyncio.coroutine
-    def _switch_test_menu(self):
-        pass
-
-    @asyncio.coroutine
-    def _coil_test_menu(self):
-        pass
-
-    @asyncio.coroutine
-    def _settings_menu(self):
-        pass
+            if key == "ENTER":
+                # start main menu
+                yield from self._start_main_menu()
+            elif key == "UP":
+                # post event for mc to increase volume
+                self.machine.events.post("master_volume_increase")
+            elif key == "DOWN":
+                # post event for mc to decrease volume
+                self.machine.events.post("master_volume_decrease")
 
     @asyncio.coroutine
     def _start_main_menu(self):
         self.machine.service.start_service()
+        self.machine.events.post("service_main_menu")
         try:
             yield from self._service_mode_main_menu()
         except asyncio.CancelledError:
@@ -113,3 +112,15 @@ down_events: list|str|
                 # call submenu
                 yield from items[position].callback()
                 self._update_main_menu(items, position)
+
+    @asyncio.coroutine
+    def _switch_test_menu(self):
+        pass
+
+    @asyncio.coroutine
+    def _coil_test_menu(self):
+        pass
+
+    @asyncio.coroutine
+    def _settings_menu(self):
+        pass
