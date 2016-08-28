b076a6d4e69d77535338999b59e8f339916c5c3d
Jan Kantert <jan-mpf@kantert.net>
2016-08-28 20:39:29 +0200

    handle ints from yaml

--- mpf/core/utility_functions.py
+++ mpf/core/utility_functions.py
@@ -423,7 +423,7 @@ class Util(object):
         Example usage: Send "c" as source_hex, returns "0C".
 
         """
-        if len(source_hex) > num_chars:
+        if len(str(source_hex)) > num_chars:
             raise ValueError("Hex string is too long.")
 
         return str(source_hex).upper().zfill(num_chars)
