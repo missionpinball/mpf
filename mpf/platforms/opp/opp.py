1fac29dcaac922bafc5e4b00a676cb84b5bfbfb4
Jan Kantert <jan-mpf@kantert.net>
2016-10-04 23:42:36 +0200

    remove old comment

--- mpf/platforms/opp/opp.py
+++ mpf/platforms/opp/opp.py
@@ -697,10 +697,9 @@ class HardwarePlatform(MatrixLightsPlatform, LedPlatform, SwitchPlatform, Driver
             return coil.config['hold_power16']
         elif coil.config['hold_power']:
             if coil.config['hold_power'] >= 8:
-                # OPP supports a maximum 15/16ms hold power
                 return 16
             else:
-                # hold_power is 0-8 and OPP supports 0-15
+                # hold_power is 0-8 and OPP supports 0-16
                 return coil.config['hold_power'] * 2
         elif coil.config['allow_enable']:
             return 16
