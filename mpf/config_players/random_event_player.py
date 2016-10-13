17a632f3eace17abe2273e7cacd93a51461e7d6d
Jan Kantert <jan-mpf@kantert.net>
2016-10-13 02:28:06 +0200

    use randomizer in random_event_player

--- mpf/config_players/random_event_player.py
+++ mpf/config_players/random_event_player.py
@@ -1,11 +1,12 @@
 """Random event config player."""
 import random
 
-from mpf.config_players.flat_config_player import FlatConfigPlayer
+from mpf.core.config_player import ConfigPlayer
+from mpf.core.randomizer import Randomizer
 from mpf.core.utility_functions import Util
 
 
-class RandomEventPlayer(FlatConfigPlayer):
+class RandomEventPlayer(ConfigPlayer):
 
     """Plays a random event based on config."""
 
@@ -13,23 +14,40 @@ class RandomEventPlayer(FlatConfigPlayer):
     show_section = 'random_events'
     device_collection = None
 
-    def play(self, settings, context, priority=0, **kwargs):
+    def __init__(self, machine):
+        super().__init__(machine)
+        self._machine_wide_dict = {}
+
+    def _get_randomizer(self, settings, context, calling_context):
+        key = "random_{}.{}".format(context, calling_context)
+        if settings['scope'] == "player":
+            if not self.machine.game.player[key]:
+                self.machine.game.player[key] = Randomizer(settings['events'])
+            return self.machine.game.player[key]
+        else:
+            if key not in self._machine_wide_dict:
+                self._machine_wide_dict[key] = Randomizer(settings['events'])
+            return self._machine_wide_dict[key]
+
+    def play(self, settings, context, calling_context, priority=0, **kwargs):
         """Play a random event from list based on config."""
-        del context
         del priority
-        self.machine.events.post(random.choice(settings['event_list']), **kwargs)
+        randomizer = self._get_randomizer(settings, context, calling_context)
+        next = randomizer.get_next()
+        self.machine.events.post(next, **kwargs)
+
+    def validate_config_entry(self, settings, name):
+        """Validate one entry of this player."""
+        config = self._parse_config(settings, name)
+        return config
 
     def get_express_config(self, value):
         """Parse express config."""
-        return dict(event_list=Util.string_to_list(value))
+        return {"events": self.get_list_config(Util.string_to_list(value))}
 
     def get_list_config(self, value):
         """Parse list."""
-        return dict(event_list=value)
-
-    def get_full_config(self, value):
-        """No further validation in here."""
-        return value
+        return {"events": value}
 
 
 player_cls = RandomEventPlayer
