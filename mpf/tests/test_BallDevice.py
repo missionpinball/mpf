7bceb34952d410be30b5b74928fe39de3a8fe1c3
Jan Kantert <jan-mpf@kantert.net>
2016-10-18 07:02:45 +0200

    slight timing change

--- mpf/tests/test_BallDevice.py
+++ mpf/tests/test_BallDevice.py
@@ -1794,7 +1794,7 @@ class TestBallDevice(MpfTestCase):
         self.advance_time_and_run(.1)
         self.machine.switch_controller.process_switch(
             "s_ball_switch_target3_2", 1)
-        self.advance_time_and_run(.5)
+        self.advance_time_and_run(.6)
         self.assertEqual(2, self._captured)
         self.assertEqual(0, playfield.balls)
         self.assertEqual(2, playfield.available_balls)
