baaf4671122f0caaa1e79c61965545adf4d76300
toomanybrians <brian@missionpinball.com>
2016-11-04 14:33:12 -0700

    updated events docstrings

--- mpf/core/mode.py
+++ mpf/core/mode.py
@@ -302,7 +302,7 @@ class Mode(object):
                                        callback=self._stopped)
         '''event: mode_(name)_stopping
 
-        The mode called "name" is stopping. This is a queue event. The
+        desc: The mode called "name" is stopping. This is a queue event. The
         mode won't actually stop until the queue is cleared.
 
         '''
