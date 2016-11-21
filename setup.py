ed5f5fb95f2b2204e72880a28ed7319dbf34cdfc
toomanybrians <brian@missionpinball.com>
2016-11-21 10:13:52 -0800

    change missionpinball.org links from https to http

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ developing it and checking in several commits a week. It's MIT licensed,
 actively developed by fun people, and supported by a vibrant, pinball-loving
 community.''',
 
-    url='https://missionpinball.org',
+    url='http://missionpinball.org',
     author='The Mission Pinball Framework Team',
     author_email='brian@missionpinball.org',
     license='MIT',
