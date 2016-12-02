af3bb6a7b8d5faeb9a414f3f11c88c1d37a5cbbd
Brian Madden <brian@missionpinball.com>
2016-12-01 21:32:22 -0800

    version bump to 0.33.0-dev.0

--- mpf/_version.py
+++ mpf/_version.py
@@ -10,8 +10,8 @@ PyPI.
 
 """
 
-__version__ = '0.32.0-dev11'
-__short_version__ = '0.32'
+__version__ = '0.33.0-dev.0'
+__short_version__ = '0.33'
 __bcp_version__ = '1.0'
 __config_version__ = '4'
 __show_version__ = '4'
