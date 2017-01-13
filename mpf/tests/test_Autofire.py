66518ca401e158da3ea58496537210aafe891d45
Jan Kantert <jan-mpf@kantert.net>
2017-01-13 16:07:20 +0100

    improve tests

--- mpf/tests/test_Autofire.py
+++ mpf/tests/test_Autofire.py
@@ -25,7 +25,8 @@ class TestAutofire(MpfTestCase):
         self.machine.autofires.ac_test.disable()
 
         self.machine.default_platform.clear_hw_rule.assert_called_once_with(
-            self.machine.autofires.ac_test.switch, self.machine.autofires.ac_test.coil)
+            self.machine.autofires.ac_test.switch.get_configured_switch(),
+            self.machine.autofires.ac_test.coil.get_configured_driver())
 
     def test_hw_rule_pulse_inverted_switch(self):
         self.machine.default_platform.set_pulse_on_hit_rule = MagicMock()
@@ -43,7 +44,8 @@ class TestAutofire(MpfTestCase):
         self.machine.autofires.ac_test_inverted.disable()
 
         self.machine.default_platform.clear_hw_rule.assert_called_once_with(
-            self.machine.autofires.ac_test_inverted.switch, self.machine.autofires.ac_test_inverted.coil)
+            self.machine.autofires.ac_test_inverted.switch.get_configured_switch(),
+            self.machine.autofires.ac_test_inverted.coil.get_configured_driver())
 
     def test_hw_rule_pulse_inverted_autofire(self):
         self.machine.default_platform.set_pulse_on_hit_rule = MagicMock()
@@ -61,4 +63,5 @@ class TestAutofire(MpfTestCase):
         self.machine.autofires.ac_test_inverted2.disable()
 
         self.machine.default_platform.clear_hw_rule.assert_called_once_with(
-            self.machine.autofires.ac_test_inverted2.switch, self.machine.autofires.ac_test_inverted2.coil)
+            self.machine.autofires.ac_test_inverted2.switch.get_configured_switch(),
+            self.machine.autofires.ac_test_inverted2.coil.get_configured_driver())
