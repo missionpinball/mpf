e32edcc7971ded8a355a2e2a597e96c6d224dba2
Jan Kantert <jan-mpf@kantert.net>
2017-01-29 21:34:07 +0100

    some more defines

--- mpf/platforms/spike/spike_defines.py
+++ mpf/platforms/spike/spike_defines.py
@@ -6,10 +6,8 @@ class SpikeNodebus:
     """Spike message defines."""
 
     Poll = 0x00
-    GetCoilReflexInfo = 0x10    # unused
     GetInputState = 0x11
     GetInputMask = 0x12         # unused
-    SetCoilReflex = 0x18        # unused; Used in CoilConfig/CoilReflex/CoilReflexHold
     SSPWrite = 0x20             # unused
     SSPRead = 0x21              # unused
     StepperStatus = 0x30        # unused
@@ -18,10 +16,12 @@ class SpikeNodebus:
     StepperHome = 0x34          # unused
     StepperInfo = 0x38          # unused
     CoilTrigger = 0x40
+    CoilSetReflex = 0x41        # unused; len: 0x18, Used in CoilConfig/CoilReflex/CoilReflexHold
     CoilGetPWMFreq = 0x42       # unused; len: 1?
     CoilSetPriority = 0x43      # unused
     CoilGetPriority = 0x43      # unused; dunno if this is right
-    SetAC = 0x50                # unused
+    CoilSetOCIgnoreMap = 0x44   # unused; len: 3
+    SetAC = 0x50                # unused; len: 4
     MotorConfigure = 0x51       # unused
     MotorStatus = 0x52          # unused
     MotorHome = 0x53            # unused; len: 6
@@ -38,11 +38,12 @@ class SpikeNodebus:
     LCDSet = 0xF2               # unused
     SendKey = 0xF3              # does something with CRC32
     SendData = 0xF4             # unused; not sure
-    INIT_BOARD = 0xF5
+    INIT_BOARD = 0xF5           # name and funtion unknown. no response
     GetDebug = 0xF6             # unused; maybe also EraseFlash
     WriteFlash = 0xF7           # unused; maybe also wrong
     GetBootStatus = 0xF8        # unused
-    GetFullBoardId = 0xF9       # unused; len: 2, probably reads 0x10 bytes back
+    GetFullBoardId = 0xF9       # unused; len: 2, reads 0x10 bytes back
+    GetCoilReflexInfo = 0xF9    # unused; len: 2, reads 0x10 bytes back
     GetCoilCurrent = 0xFA
     DisableOC = 0xFB            # unused
     GetBoardId = 0xFD           # unused; len: 1, probably reads 0x0C bytes back
