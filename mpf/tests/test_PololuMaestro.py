d43fc13f76dfb41fbbc41a28402ccbf47f23f462
Jan Kantert <jan-mpf@kantert.net>
2017-03-03 14:30:16 +0100

    fix test and config loading for pololu_maestro

--- mpf/tests/test_PololuMaestro.py
+++ mpf/tests/test_PololuMaestro.py
@@ -11,8 +11,10 @@ class TestPololuMaestro(MpfTestCase):
     def getMachinePath(self):
         return 'tests/machine_files/pololu_maestro/'
 
-    def get_platform(self):
-        return 'pololu_maestro'
+    def getOptions(self):
+        options = super().getOptions()
+        options['force_platform'] = False
+        return options
 
     def setUp(self):
         self.serial = MagicMock()
