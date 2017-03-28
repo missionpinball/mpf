7e2f51f9b4ef48cd6044ec77213a9d9cc7dafc26
Jan Kantert <jan-mpf@kantert.net>
2017-03-28 14:54:22 +0200

    only assert in unit tests to allow fuzzing

--- mpf/platforms/smart_virtual.py
+++ mpf/platforms/smart_virtual.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from mpf.core.logging import LogMixin
 from mpf.core.platform import DriverConfig
 
 from mpf.platforms.interfaces.driver_platform_interface import PulseSettings, HoldSettings
@@ -321,7 +322,7 @@ class HardwarePlatform(VirtualPlatform):
 
     def add_ball_to_device(self, device):
         """Add ball to device."""
-        if device.balls >= device.config['ball_capacity']:
+        if LogMixin.unit_test and device.balls >= device.config['ball_capacity']:
             raise AssertionError("KABOOM! We just added a ball to {} which has a capacity "
                                  "of {} but already had {} ball(s)".format(device.name,
                                                                            device.config['ball_capacity'],
@@ -336,7 +337,7 @@ class HardwarePlatform(VirtualPlatform):
             if device.config['entrance_switch_full_timeout']:
                 if device.balls == device.config['ball_capacity'] - 1:
 
-                    if self.machine.switch_controller.is_active(
+                    if LogMixin.unit_test and self.machine.switch_controller.is_active(
                             device.config['entrance_switch'].name):
                         raise AssertionError(
                             "KABOOM! We just added a ball to {} which already "
@@ -361,7 +362,7 @@ class HardwarePlatform(VirtualPlatform):
                     found_switch = True
                     break
 
-            if not found_switch:
+            if LogMixin.unit_test and not found_switch:
                 raise AssertionError("KABOOM! We just added a ball to {} which"
                                      "was already full.".format(device.name))
 
