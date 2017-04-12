45ded650e62713565258c6f5ede4a9afae824c16
Jan Kantert <jan-mpf@kantert.net>
2017-04-12 17:55:43 +0200

    better RGBAColor

--- mpf/core/rgba_color.py
+++ mpf/core/rgba_color.py
@@ -1,5 +1,5 @@
 """RGBA Color."""
-from typing import Tuple, Union
+from typing import Tuple, Union, List
 
 from mpf.core.rgb_color import RGBColor
 
@@ -8,14 +8,14 @@ class RGBAColor(RGBColor):
 
     """RGB Color with alpha channel."""
 
-    def __init__(self, color: Union[RGBColor, Tuple[int, int, int, int]]):
+    def __init__(self, color: Union[RGBColor, str, List[int], Tuple[int, int, int], Tuple[int, int, int, int]]):
         """Initialise RGBA color."""
-        if isinstance(color, RGBColor):
-            self.opacity = 255
-            super().__init__(color)
-        else:
+        if isinstance(color, tuple) and len(color) == 4:
             self.opacity = color[3]
             super().__init__((color[0], color[1], color[2]))
+        else:
+            self.opacity = 255
+            super().__init__(color)
 
     def __iter__(self):
         """Return iterator."""
