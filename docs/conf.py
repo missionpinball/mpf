800cf751a4de81ec226ddc55a7258e7ed05f8dc0
Brian Madden <brian@missionpinball.com>
2017-05-03 19:50:23 -0500

    add "mpf" as module index prefix for API docs

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+modindex_common_prefix = ['mpf']
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
