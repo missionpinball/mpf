3e0903f523d1f6fad000541de3bdaf8d4c4730cb
Jan Kantert <jan-mpf@kantert.net>
2017-06-08 22:38:56 +0200

    improve var docstring

--- mpf/modes/game/code/game.py
+++ mpf/modes/game/code/game.py
@@ -384,17 +384,17 @@ class Game(AsyncMode):
                     value=player.score)
                 '''machine_var: player(x)_score
     
-                desc: Holds the numeric value of a player's score. The "x" is the
-                player number, so this actual machine variable is
-                ``player1_score`` or ``player2_score``.
+                desc: Holds the numeric value of a player's score from the last
+                game. The "x" is the player number, so this actual machine
+                variable is ``player1_score`` or ``player2_score``.
     
                 Since these are machine variables, they are maintained even after
                 a game is over. Therefore you can use these machine variables in
                 your attract mode display show to show the scores of the last game
                 that was played.
     
-                These machine variables are updated at the end of each player's
-                turn, and they persist on disk so they are restored the next time
+                These machine variables are updated at the end of the game,
+                and they persist on disk so they are restored the next time
                 MPF starts up.
     
                 '''
