87cf3c3a4665902952af9b1b0bde781dcedc62f2
Jan Kantert <jan-mpf@kantert.net>
2017-07-12 00:58:43 +0200

    fix device loading order with machine devices

--- mpf/core/device_manager.py
+++ mpf/core/device_manager.py
@@ -23,8 +23,13 @@ class DeviceManager(MpfController):
         self.collections = OrderedDict()
         self.device_classes = OrderedDict()  # collection_name: device_class
 
+        # this has to happen before mode load (which is priority 10)
         self.machine.events.add_handler('init_phase_1',
-                                        self._load_device_modules)
+                                        self._load_device_config_spec, priority=20)
+
+        # this has to happen after mode load
+        self.machine.events.add_handler('init_phase_1',
+                                        self._load_device_modules, priority=5)
 
         self.machine.events.add_handler('init_phase_2',
                                         self.create_machinewide_device_control_events,
@@ -60,10 +65,8 @@ class DeviceManager(MpfController):
         """
         self.machine.bcp.interface.notify_device_changes(device, notify, old, value)
 
-    def _load_device_modules(self, **kwargs):
+    def _load_device_config_spec(self, **kwargs):
         del kwargs
-        # step 1: create devices in machine collection
-        self.debug_log("Creating devices...")
         for device_type in self.machine.config['mpf']['device_modules']:
             device_cls = Util.string_to_class(device_type)      # type: Device
 
@@ -71,6 +74,13 @@ class DeviceManager(MpfController):
                 # add specific config spec if device has any
                 self.machine.config_validator.load_device_config_spec(
                     device_cls.config_section, device_cls.get_config_spec())
+
+    def _load_device_modules(self, **kwargs):
+        del kwargs
+        # step 1: create devices in machine collection
+        self.debug_log("Creating devices...")
+        for device_type in self.machine.config['mpf']['device_modules']:
+            device_cls = Util.string_to_class(device_type)      # type: Device
 
             collection_name, config = device_cls.get_config_info()
 
