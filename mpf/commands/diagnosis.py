7b152263bf9061251b46e0b10c8d9c4bb5f1683b
Brian Madden <brian@missionpinball.com>
2017-07-13 09:33:41 -0500

    Add more things to diagnosis command

--- mpf/commands/diagnosis.py
+++ mpf/commands/diagnosis.py
@@ -11,11 +11,16 @@ class Command(object):
 
     def __init__(self, mpf_path, machine_path, args):
         """Run mpf diagnosis."""
-        del mpf_path, machine_path, args
+        del args
         print("MPF version: {}".format(mpf_version))
+        print("MPF install location: {}".format(mpf_path))
+        print("Machine folder detected: {}".format(machine_path))
+
         try:
             from mpfmc._version import version as mc_version
             print("MPF-MC version: {}".format(mc_version))
+
         except:
             print("MPF-MC not found")
+
         sys.exit()
