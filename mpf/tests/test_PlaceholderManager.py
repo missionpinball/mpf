65a9ed49cef2dcf3b6f326aee83b4797a00e33c6
Jan Kantert <jan-mpf@kantert.net>
2017-08-18 07:25:22 +0000

    adjust test

--- mpf/tests/test_PlaceholderManager.py
+++ mpf/tests/test_PlaceholderManager.py
@@ -1,5 +1,6 @@
 """Test placeholders."""
 import unittest
+from mpf.tests.MpfFakeGameTestCase import MpfFakeGameTestCase
 from unittest.mock import MagicMock
 
 from mpf.core.placeholder_manager import PlaceholderManager
@@ -14,3 +15,27 @@ class TestPlaceholderManager(unittest.TestCase):
         # test mod operator
         template = p.build_int_template("a % 7", None)
         self.assertEqual(3, template.evaluate({"a": 10}))
+
+class TestPlaceholderInGame(MpfFakeGameTestCase):
+
+    def getConfigFile(self):
+        return 'config.yaml'
+
+    def getMachinePath(self):
+        return 'tests/machine_files/placeholder/'
+
+    def get_platform(self):
+        return 'smart_virtual'
+
+    def test_player_vars_and_conditionals(self):
+        self.start_game()
+        self.assertPlayerVarEqual(4, "test_int")
+        self.assertPlayerVarEqual("Test", "test_str")
+
+        self.mock_event("test_yes")
+        self.mock_event("test_no")
+        self.post_event("test_conditional")
+
+        self.assertEventCalled("test_yes")
+        self.assertEventNotCalled("test_no")
+
