3a4edfcba23bb3e81cd658052c6f093770c3b743
Jan Kantert <jan-mpf@kantert.net>
2017-11-05 13:00:40 +0100

    always mark playfield active

--- mpf/devices/shot.py
+++ mpf/devices/shot.py
@@ -248,11 +248,12 @@ class Shot(EnableDisableMixin, ModeDevice):
         """
         del kwargs
 
+        # mark the playfield active no matter what
+        self.config['playfield'].mark_playfield_active_from_device_action()
+
         if not self.enabled:
             return
 
-        # mark the playfield active no matter what
-        self.config['playfield'].mark_playfield_active_from_device_action()
         # Stop if there is an active delay but no sequence
         if self.active_delays:
             return
