a74ae526a30ec5d474e06aaa40e51ae6793d0657
Jan Kantert <jan-mpf@kantert.net>
2017-12-24 13:49:52 +0100

    verify settings for pulse and enable

--- mpf/platforms/fast/fast_driver.py
+++ mpf/platforms/fast/fast_driver.py
@@ -1,6 +1,6 @@
 """A driver/coil in the fast platform."""
 import logging
-from typing import Dict
+from typing import Dict, Tuple
 
 from mpf.core.platform import DriverConfig
 from mpf.core.utility_functions import Util
@@ -20,11 +20,12 @@ class FASTDriver(DriverPlatformInterface):
         """Initialise driver."""
         super().__init__(config, number)
         self.log = logging.getLogger('FASTDriver')
-        self.autofire = None                        # type: str
+        self.autofire = None                        # type: Tuple[str, Dict[str, float]]
+        self._autofire_cleared = False
         self.config_state = None
         self.machine = platform.machine
         self.platform = platform
-        self.driver_settings = dict()               # type: Dict[str, str]
+        self.driver_settings = dict()               # type: Dict[str, float]
         self.send = platform.net_connection.send
         self.platform_settings = platform_settings
 
@@ -105,17 +106,43 @@ class FASTDriver(DriverPlatformInterface):
 
         self.log.debug("Sending Disable Command: %s", cmd)
         self.send(cmd)
-        self.check_auto()
+
+        self._reenable_autofire_if_configured()
+
+        # reenable the autofire
+        if self.autofire:
+            cmd = '{}{},00'.format(self.get_trigger_cmd(), self.number)
+
+            self.log.debug("Re-enabling auto fire mode: %s", cmd)
+            self.send(cmd)
+
+    def set_autofire(self, autofire_cmd, pulse_duration, pulse_power, hold_power):
+        """Set an autofire."""
+        self.autofire = autofire_cmd, (pulse_duration, pulse_power, hold_power)
+        self.config_state = pulse_duration, pulse_power, hold_power
+        self._autofire_cleared = False
+        self.log.debug("Writing hardware rule: %s", autofire_cmd)
+        self.send(autofire_cmd)
+
+    def clear_autofire(self, config_cmd, number):
+        """Clear autofire."""
+        cmd = '{}{},81'.format(config_cmd, number)
+        self.log.debug("Clearing hardware rule: %s", cmd)
+        self.send(cmd)
+        self.autofire = None
+        self.config_state = None
 
     def enable(self, pulse_settings: PulseSettings, hold_settings: HoldSettings):
         """Enable (turn on) this driver."""
-        if self.autofire:
+        config_state = pulse_settings.duration, pulse_settings.power, hold_settings.power
+        if self.autofire and self.config_state == config_state:
             # If this driver is also configured for an autofire rule, we just
             # manually trigger it with the trigger_cmd and manual on ('03')
             cmd = '{}{},03'.format(self.get_trigger_cmd(), self.number)
         else:
             # Otherwise we send a full config command, trigger C1 (logic triggered
             # and drive now) switch ID 00, mode 18 (latched)
+            self._autofire_cleared = True
 
             cmd = '{}{},C1,00,18,{},{},{},{}'.format(
                 self.get_config_cmd(),
@@ -125,7 +152,7 @@ class FASTDriver(DriverPlatformInterface):
                 self.get_pwm_for_cmd(hold_settings.power),
                 self.get_recycle_ms_for_cmd(self.config.default_recycle, pulse_settings.duration)
             )
-            self.config_state = None
+            self.config_state = pulse_settings
 
         self.log.debug("Sending Enable Command: %s", cmd)
         self.send(cmd)
@@ -133,11 +160,12 @@ class FASTDriver(DriverPlatformInterface):
     def pulse(self, pulse_settings: PulseSettings):
         """Pulse this driver."""
         hex_ms_string = Util.int_to_hex_string(pulse_settings.duration)
-        config_state = (pulse_settings.duration, pulse_settings.power)
+        config_state = (pulse_settings.duration, pulse_settings.power, 0)
 
         # reconfigure if we have to
-        if self.config_state != config_state:
+        if not self.config_state or self.config_state[0] != config_state[0] or self.config_state[1] != config_state[1]:
             self.config_state = config_state
+            self._autofire_cleared = True
 
             cmd = '{}{},81,00,10,{},{},00,00,00'.format(
                 self.get_config_cmd(),
@@ -151,16 +179,16 @@ class FASTDriver(DriverPlatformInterface):
         self.send(cmd)
 
         # restore autofire
-        if self.autofire:
-            self.config_state = None
-            self.send(self.autofire)
+        self._reenable_autofire_if_configured()
 
         return Util.hex_string_to_int(hex_ms_string)
 
-    def check_auto(self):
+    def _reenable_autofire_if_configured(self):
         """Reenable autofire if configured."""
-        if self.autofire:
-            cmd = '{}{},00'.format(self.get_trigger_cmd(), self.number)
+        if self.autofire and self._autofire_cleared:
+            self._autofire_cleared = False
+            cmd = self.autofire[0]
+            self.config_state = self.autofire[1]
 
             self.log.debug("Re-enabling auto fire mode: %s", cmd)
             self.send(cmd)
