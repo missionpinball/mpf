8a7f42a0fc0b67efbf595dccf27cea471bd69973
Jan Kantert <jan-mpf@kantert.net>
2018-01-28 17:20:44 +0100

    cs

--- mpf/core/platform.py
+++ mpf/core/platform.py
@@ -48,6 +48,7 @@ class BasePlatform(metaclass=abc.ABCMeta):
         self.features['hardware_sounds'] = False
         self.features['has_steppers'] = False
 
+    # pylint: disable-msg=no-self-use
     def get_info_string(self) -> str:
         """Return information string about this platform."""
         return "Not implemented"
