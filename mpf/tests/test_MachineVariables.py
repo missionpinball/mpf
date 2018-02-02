985afc1e2bf7c794ea9f07c3c84a9c06fa138ac4
Jan Kantert <jan-mpf@kantert.net>
2018-02-02 18:46:49 +0100

    fix persist to disk and cleanup DataManager

--- mpf/tests/test_MachineVariables.py
+++ mpf/tests/test_MachineVariables.py
@@ -7,11 +7,20 @@ from mpf._version import version, extended_version
 
 class TestMachineVariables(MpfTestCase):
 
+    def getConfigFile(self):
+        return 'config.yaml'
+
+    def getMachinePath(self):
+        return 'tests/machine_files/machine_vars/'
+
     def _get_mock_data(self):
         return {"machine_vars": {"player2_score": {"value": 118208660},
                                  "player3_score": {"value": 17789290},
                                  "player4_score": {"value": 3006600},
-                                 "another_score": {"value": 123}},
+                                 "another_score": {"value": 123},
+                                 "expired_value": {"value": 23, "expire": self.clock.get_time() - 100},
+                                 "not_expired_value": {"value": 24, "expire": self.clock.get_time() + 100},
+                                 "test1": {"value": 42}},
                 }
 
     def testSystemInfoVariables(self):
@@ -27,7 +36,18 @@ class TestMachineVariables(MpfTestCase):
         self.assertEqual(extended_version, self.machine.get_machine_var("mpf_extended_version"))
 
     def testVarLoadAndRemove(self):
+        self.assertFalse(self.machine.is_machine_var("expired_value"))
+        self.assertTrue(self.machine.is_machine_var("not_expired_value"))
         self.assertTrue(self.machine.is_machine_var("player2_score"))
+        # should always persist
+        #self.assertTrue(self.machine.machine_vars["player2_score"]["persist"])
+        # random variable does not persist
+        self.assertFalse(self.machine.machine_vars["another_score"]["persist"])
+        # configured to persist
+        self.assertTrue(self.machine.machine_vars["test1"]["persist"])
+        self.assertTrue(self.machine.machine_vars["test2"]["persist"])
+        # configured to not persist
+        self.assertFalse(self.machine.machine_vars["test3"]["persist"])
         self.assertEqual(118208660, self.machine.get_machine_var("player2_score"))
 
         self.machine.remove_machine_var("player2_score")
@@ -45,7 +65,8 @@ class TestMachineVariables(MpfTestCase):
         self.advance_time_and_run(10)
 
         self.machine.machine_var_data_manager._trigger_save.assert_called_with()
-        self.assertEqual({'another_score': {'value': 123}}, self.machine.machine_var_data_manager.data)
+        self.assertEqual({'test1': {'value': 42, 'expire': None}, 'test2': {'value': '5', 'expire': None}},
+                         self.machine.machine_var_data_manager.data)
 
 
 class TestMalformedMachineVariables(MpfTestCase):
