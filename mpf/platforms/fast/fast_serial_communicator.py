e52f863235ac5806662725d76f2e0ae2885c272a
Jan Kantert <jan-mpf@kantert.net>
2018-03-03 21:37:57 +0100

    new reset command

--- mpf/platforms/fast/fast_serial_communicator.py
+++ mpf/platforms/fast/fast_serial_communicator.py
@@ -65,7 +65,7 @@ class FastSerialCommunicator(BaseSerialCommunicator):
 
         self.send_queue = asyncio.Queue(loop=platform.machine.clock.loop)
 
-        super().__init__(platform, port, baud, True)
+        super().__init__(platform, port, baud)
 
     def stop(self):
         """Stop and shut down this serial connection."""
@@ -181,8 +181,12 @@ class FastSerialCommunicator(BaseSerialCommunicator):
         """
         # reset CPU early
         self.platform.debug_log('Resetting NET CPU.')
-        self.writer.write('BL:AA55\r'.encode())
-        yield from asyncio.sleep(4, loop=self.machine.clock.loop)
+        self.writer.write('BC:\r'.encode())
+        msg = ''
+        while not msg.startswith('!B:02\r'):
+            msg = (yield from self.readuntil(b'\r')).decode()
+        if not msg.startswith('!B:'):
+            self.platform.log.warning("Got unexpected message from FAST: {}".format(msg))
 
         self.platform.debug_log('Reading all switches.')
         self.writer.write('SA:\r'.encode())
