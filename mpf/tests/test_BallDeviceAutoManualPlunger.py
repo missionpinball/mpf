30a5b58bf0469acb7647a7bc9289347c0852763f
Jan Kantert <jan-mpf@kantert.net>
2018-03-15 22:45:15 +0100

    only handle lost balls outside of ejects

--- mpf/tests/test_BallDeviceAutoManualPlunger.py
+++ mpf/tests/test_BallDeviceAutoManualPlunger.py
@@ -110,3 +110,57 @@ class TestBallDeviceAutoManualPlunger(MpfTestCase):
 
         self.assertEqual(0, plunger.balls)
         self.assertEqual(1, playfield.balls)
+
+    def test_returned_ball_with_missing_ball(self):
+        plunger = self.machine.ball_devices['plunger']
+        trough = self.machine.ball_devices['trough']
+        playfield = self.machine.ball_devices['playfield']
+        trough_coil = self.machine.coils['trough_eject']
+        plunger_coil = self.machine.coils['plunger_eject']
+        trough_coil.pulse = MagicMock()
+        plunger_coil.pulse = MagicMock()
+
+        # add ball to trough
+        self.machine.switch_controller.process_switch('s_trough_1', 1)
+        self.machine.switch_controller.process_switch('s_trough_2', 1)
+
+        self.advance_time_and_run(1)
+        self.assertEqual(0, playfield.balls)
+        self.assertEqual(0, plunger.balls)
+        assert not plunger_coil.pulse.called
+        assert not trough_coil.pulse.called
+
+        # Request player eject to pf
+        playfield.add_ball(player_controlled=False)
+        self.advance_time_and_run(1)
+
+        self.assertTrue(trough_coil.pulse.called)
+        assert not plunger_coil.pulse.called
+
+        self.machine.switch_controller.process_switch('s_trough_1', 0)
+        self.advance_time_and_run(1)
+
+        # plunger gets ball
+        self.machine.switch_controller.process_switch('s_plunger', 1)
+        self.advance_time_and_run(1)
+        self.assertEqual(1, plunger.balls)
+        self.assertTrue(plunger_coil.pulse.called)
+        self.advance_time_and_run(1)
+
+        self.machine.switch_controller.process_switch('s_plunger', 0)
+        self.advance_time_and_run(1)
+
+        # ball returns
+        self.machine.switch_controller.process_switch('s_plunger', 1)
+        self.advance_time_and_run(9)
+        # but then disappears
+        self.machine.switch_controller.process_switch('s_plunger', 0)
+        self.advance_time_and_run(20)
+
+        self.assertEqual(0, plunger.balls)
+        self.assertFalse(plunger.outgoing_balls_handler._eject_queue.qsize())
+        self.assertEqual("idle", plunger.state)
+        self.assertEqual(1, playfield.balls)
+        self.assertFalse(trough.outgoing_balls_handler._eject_queue.qsize())
+        self.assertEqual("idle", trough.state)
+        self.assertBallsOnPlayfield(1)
