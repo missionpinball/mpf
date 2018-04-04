d0308c6e5835aeb33bc593b1f42978cac44cab2b
Jan Kantert <jan-mpf@kantert.net>
2018-04-04 23:17:08 +0200

    convert smbus2 to asyncio

--- mpf/platforms/smbus2.py
+++ mpf/platforms/smbus2.py
@@ -2,14 +2,16 @@
 import asyncio
 from typing import Dict, Tuple
 
+from mpf.core.utility_functions import Util
+
 from mpf.core.platform import I2cPlatform
 
 extension_loaded = True
 # pylint: disable-msg=ungrouped-imports
 try:
-    from smbus2 import SMBus
+    from smbus2_asyncio import SMBus2Asyncio
 except ImportError:     # pragma: no cover
-    SMBus = None
+    SMBus2Asyncio = None
 
 
 class Smbus2(I2cPlatform):
@@ -19,13 +21,13 @@ class Smbus2(I2cPlatform):
     def __init__(self, machine):
         """Initialise smbus2 platform."""
         super().__init__(machine)
-        self._i2c_handles = {}  # type: Dict[int, SMBus]
+        self._i2c_handles = {}  # type: Dict[int, SMBus2Asyncio]
 
     @asyncio.coroutine
     def initialize(self):
         """Check if smbus2 extension has been imported."""
-        if not SMBus:
-            raise AssertionError("smbus2 python extension missing. Please run: pip3 install smbus2")
+        if not SMBus2Asyncio:
+            raise AssertionError("smbus2 python extension missing. Please run: pip3 install smbus2_asyncio")
 
     @staticmethod
     def _get_i2c_bus_address(address) -> Tuple[int, int]:
@@ -35,11 +37,12 @@ class Smbus2(I2cPlatform):
         bus, address = address.split("-")
         return bus, int(address)
 
-    def _get_i2c_bus(self, bus) -> SMBus:
+    def _get_i2c_bus(self, bus) -> SMBus2Asyncio:
         """Get or open handle for i2c bus."""
         if bus in self._i2c_handles:
             return self._i2c_handles[bus]
-        handle = SMBus(bus)
+        handle = SMBus2Asyncio(bus, loop=self.machine.clock.loop)
+        handle.open_sync()
         self._i2c_handles[bus] = handle
         return handle
 
@@ -48,17 +51,18 @@ class Smbus2(I2cPlatform):
         """Read a byte from I2C."""
         bus, address = self._get_i2c_bus_address(address)
         smbus = self._get_i2c_bus(bus)
-        return smbus.read_byte_data(address, int(register))
+        return (yield from smbus.read_byte_data(address, int(register)))
 
     def i2c_write8(self, address, register, value):
         """Write a byte to I2C."""
         bus, address = self._get_i2c_bus_address(address)
         smbus = self._get_i2c_bus(bus)
-        return smbus.write_byte_data(address, int(register), int(value))
+        Util.ensure_future(smbus.write_byte_data(address, int(register), int(value)), loop=self.machine.clock.loop)
+        # this does not return
 
     @asyncio.coroutine
     def i2c_read_block(self, address, register, count):
         """Read a block from I2C."""
         bus, address = self._get_i2c_bus_address(address)
         smbus = self._get_i2c_bus(bus)
-        return smbus.read_i2c_block_data(address, int(register), int(count))
+        return (yield from smbus.read_i2c_block_data(address, int(register), int(count)))
