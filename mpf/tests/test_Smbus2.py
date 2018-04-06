e2779579c24db2e8bc00318c81c348deb6451421
Jan Kantert <jan-mpf@kantert.net>
2018-04-06 16:59:11 +0200

    fix test without extension

--- mpf/tests/test_Smbus2.py
+++ mpf/tests/test_Smbus2.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from mpf.platforms import smbus2
 from mpf.tests.MpfTestCase import MpfTestCase, MagicMock, patch
 
@@ -18,15 +20,20 @@ class TestSmbus2(MpfTestCase):
         super().setUp()
 
     def test_i2c(self):
-        with patch("smbus2_asyncio.SMBus") as smbus:
+        with patch("mpf.platforms.smbus2.SMBus2Asyncio") as smbus:
             smbus_instance = MagicMock()
             smbus.return_value = smbus_instance
+            result = asyncio.Future(loop=self.loop)
+            result.set_result(True)
+            smbus_instance.write_byte_data = MagicMock(return_value=result)
 
             self.machine.default_platform.i2c_write8("1-17", 23, 1337)
             self.machine_run()
             smbus_instance.write_byte_data.assert_called_once_with(17, 23, 1337)
-            smbus.assert_called_once_with('1')
+            smbus.assert_called_once_with('1', loop=self.loop)
 
-            smbus_instance.read_byte_data = MagicMock(return_value=1337)
+            result = asyncio.Future(loop=self.loop)
+            result.set_result(1337)
+            smbus_instance.read_byte_data = MagicMock(return_value=result)
             self.assertEqual(1337, self.loop.run_until_complete(self.machine.default_platform.i2c_read8("1-17", 23)))
             smbus_instance.read_byte_data.assert_called_once_with(17, 23)
