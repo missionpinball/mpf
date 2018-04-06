a76d6d2abf64e07cc6111df1242190891219480a
Jan Kantert <jan-mpf@kantert.net>
2018-04-06 20:05:50 +0200

    mock smbus2 earlier

--- mpf/tests/test_Smbus2.py
+++ mpf/tests/test_Smbus2.py
@@ -17,23 +17,25 @@ class TestSmbus2(MpfTestCase):
         return False
 
     def setUp(self):
+        smbus = patch('mpf.platforms.smbus2.SMBus2Asyncio')
+        self.smbus = smbus.start()
+        self.smbus_instance = MagicMock()
+        self.smbus.return_value = self.smbus_instance
+        self.addCleanup(self.smbus.stop)
         super().setUp()
 
     def test_i2c(self):
-        with patch("mpf.platforms.smbus2.SMBus2Asyncio") as smbus:
-            smbus_instance = MagicMock()
-            smbus.return_value = smbus_instance
-            result = asyncio.Future(loop=self.loop)
-            result.set_result(True)
-            smbus_instance.write_byte_data = MagicMock(return_value=result)
+        result = asyncio.Future(loop=self.loop)
+        result.set_result(True)
+        self.smbus_instance.write_byte_data = MagicMock(return_value=result)
 
-            self.machine.default_platform.i2c_write8("1-17", 23, 1337)
-            self.machine_run()
-            smbus_instance.write_byte_data.assert_called_once_with(17, 23, 1337)
-            smbus.assert_called_once_with('1', loop=self.loop)
+        self.machine.default_platform.i2c_write8("1-17", 23, 1337)
+        self.machine_run()
+        self.smbus_instance.write_byte_data.assert_called_once_with(17, 23, 1337)
+        self.smbus.assert_called_once_with('1', loop=self.loop)
 
-            result = asyncio.Future(loop=self.loop)
-            result.set_result(1337)
-            smbus_instance.read_byte_data = MagicMock(return_value=result)
-            self.assertEqual(1337, self.loop.run_until_complete(self.machine.default_platform.i2c_read8("1-17", 23)))
-            smbus_instance.read_byte_data.assert_called_once_with(17, 23)
+        result = asyncio.Future(loop=self.loop)
+        result.set_result(1337)
+        self.smbus_instance.read_byte_data = MagicMock(return_value=result)
+        self.assertEqual(1337, self.loop.run_until_complete(self.machine.default_platform.i2c_read8("1-17", 23)))
+        self.smbus_instance.read_byte_data.assert_called_once_with(17, 23)
