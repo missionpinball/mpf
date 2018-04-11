f67340fd0a43fff5aca609cc3de4f71fa08859cf
Jan Kantert <jan-mpf@kantert.net>
2018-04-12 00:48:42 +0200

    mock signal handler in test

--- mpf/tests/test_Commands.py
+++ mpf/tests/test_Commands.py
@@ -8,15 +8,16 @@ from mpf.commands import game, migrate, both
 class TestCommands(TestCase):
 
     def test_game(self):
-        with patch("mpf.commands.game.logging"):
-            with patch("mpf.commands.game.os"):
-                with patch("mpf.commands.game.sys") as sys:
-                    with patch("mpf.commands.game.MachineController") as controller:
-                        with patch("asciimatics.screen.Screen.open"):
-                            game.Command("test", "machine", "")
-                            self.assertEqual("test", controller.call_args[0][0])
-                            self.assertEqual("machine", controller.call_args[0][1])
-                            sys.exit.assert_called_once_with()
+        with patch("mpf.commands.game.signal"):
+            with patch("mpf.commands.game.logging"):
+                with patch("mpf.commands.game.os"):
+                    with patch("mpf.commands.game.sys") as sys:
+                        with patch("mpf.commands.game.MachineController") as controller:
+                            with patch("asciimatics.screen.Screen.open"):
+                                game.Command("test", "machine", "")
+                                self.assertEqual("test", controller.call_args[0][0])
+                                self.assertEqual("machine", controller.call_args[0][1])
+                                sys.exit.assert_called_once_with()
 
     def test_migrate(self):
         with patch("mpf.commands.migrate.logging"):
