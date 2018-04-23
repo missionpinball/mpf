9d8df4b3a96eac27037030280ad35b44e6afed1b
Quinn Capen <qcapen@gmail.com>
2018-04-23 10:49:23 -0600

    0.51 dev-0

--- mpf/_version.py
+++ mpf/_version.py
@@ -10,10 +10,10 @@ PyPI.
 
 """
 
-__version__ = '0.50.0-dev.93'
+__version__ = '0.51.0-dev.0'
 '''The full version of MPF.'''
 
-__short_version__ = '0.50'
+__short_version__ = '0.51'
 '''The major.minor version of MPF.'''
 
 __bcp_version__ = '1.1'
