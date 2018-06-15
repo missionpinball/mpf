c7f30c953a7ae29ff1b2e31524e31304130dcce0
Jan Kantert <jan-mpf@kantert.net>
2018-06-15 13:07:44 +0200

    improve benchmark

--- mpf/benchmarks/benchmark_switch_hits.py
+++ mpf/benchmarks/benchmark_switch_hits.py
@@ -1,5 +1,7 @@
 import time
 
+from mpf.core.logging import LogMixin
+
 from mpf.tests.MpfGameTestCase import MpfGameTestCase
 
 
@@ -14,43 +16,40 @@ class BenchmarkSwitchHits(MpfGameTestCase):
     def get_platform(self):
         return 'virtual'
 
-    def testExcessiveSwitchHits(self):
+    def setUp(self):
+        LogMixin.unit_test = False
+        super().setUp()
+
+    def testPlayfieldActiveSwitchHits(self):
         for i in range(1000):
             self.machine.switch_controller.process_switch_by_num("1", 1, self.machine.default_platform)
             self.machine.switch_controller.process_switch_by_num("1", 0, self.machine.default_platform)
-
-        num = 10000
-        start = time.time()
-        for i in range(num):
-            self.machine.switch_controller.process_switch_by_num("1", 1, self.machine.default_platform)
-            self.machine.switch_controller.process_switch_by_num("1", 0, self.machine.default_platform)
-        end = time.time()
         self.machine_run()
-        print("Duration", "{:.10f}".format((end - start) / num))
 
-        num = 10000
-        start = time.time()
-        for i in range(num):
-            self.machine.switch_controller.process_switch_by_num("1", 1, self.machine.default_platform)
-            self.machine.switch_controller.process_switch_by_num("1", 0, self.machine.default_platform)
-        end = time.time()
-        self.machine_run()
-        print("Duration", "{:.10f}".format((end - start) / num))
+        for runs in range(10):
+            num = 10000
+            start = time.time()
+            for i in range(num):
+                self.machine.switch_controller.process_switch_by_num("1", 1, self.machine.default_platform)
+                self.machine.switch_controller.process_switch_by_num("1", 0, self.machine.default_platform)
+            end = time.time()
+            self.machine_run()
+            end2 = time.time()
+            print("Duration {:.5f}ms Processing {:.5f}ms".format((1000 * (end - start) / num), ((end2 - end) * 1000) / num))
 
-        num = 10000
-        start = time.time()
-        for i in range(num):
-            self.machine.switch_controller.process_switch_by_num("1", 1, self.machine.default_platform)
-            self.machine.switch_controller.process_switch_by_num("1", 0, self.machine.default_platform)
-        end = time.time()
+    def test20TagsHits(self):
+        for i in range(1000):
+            self.machine.switch_controller.process_switch_by_num("2", 1, self.machine.default_platform)
+            self.machine.switch_controller.process_switch_by_num("2", 0, self.machine.default_platform)
         self.machine_run()
-        print("Duration", "{:.10f}".format((end - start) / num))
 
-        num = 10000
-        start = time.time()
-        for i in range(num):
-            self.machine.switch_controller.process_switch_by_num("1", 1, self.machine.default_platform)
-            self.machine.switch_controller.process_switch_by_num("1", 0, self.machine.default_platform)
-        end = time.time()
-        self.machine_run()
-        print("Duration", "{:.10f}".format((end - start) / num))
+        for runs in range(10):
+            num = 10000
+            start = time.time()
+            for i in range(num):
+                self.machine.switch_controller.process_switch_by_num("2", 1, self.machine.default_platform)
+                self.machine.switch_controller.process_switch_by_num("2", 0, self.machine.default_platform)
+            end = time.time()
+            self.machine_run()
+            end2 = time.time()
+            print("Duration {:.5f}ms Processing {:.5f}ms".format((1000 * (end - start) / num), ((end2 - end) * 1000) / num))
