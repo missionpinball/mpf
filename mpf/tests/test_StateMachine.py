1239ef4c9feb27c09b39ccd53a711976e7280aed
Jan Kantert <jan-mpf@kantert.net>
2019-05-11 14:27:44 +0200

    allow monitoring of state in StateMachines

--- mpf/tests/test_StateMachine.py
+++ mpf/tests/test_StateMachine.py
@@ -10,13 +10,19 @@ class TestStateMachine(MpfFakeGameTestCase):
         return 'tests/machine_files/state_machine/'
 
     def test_state_machine(self):
+        placeholder_str = "device.state_machines.my_state.state"
+        placeholder = self.machine.placeholder_manager.build_raw_template(placeholder_str)
+        placeholder_value, placeholder_future = placeholder.evaluate_and_subscribe([])
+        self.assertPlaceholderEvaluates("start", placeholder_str)
         self.assertEqual("start", self.machine.state_machines.my_state.state)
 
         self.mock_event("step1_start")
         self.mock_event("step1_stop")
         self.mock_event("going_to_step2")
         self.post_event("state_machine_proceed")
+        self.assertPlaceholderEvaluates("step1", placeholder_str)
         self.assertEqual("step1", self.machine.state_machines.my_state.state)
+        self.assertTrue(placeholder_future.done())
         self.assertEqual("on", self.machine.state_machines.my_state._show.name)
         self.assertEventCalled("step1_start")
         self.mock_event("step1_start")
