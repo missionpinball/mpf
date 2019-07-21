2bbc750cfc27df04b83f57680fe27003484b1ef1
Jan Kantert <jan-mpf@kantert.net>
2019-07-21 13:29:29 +0200

    show a better error when configuring invalid display

--- mpf/platforms/lisy/lisy.py
+++ mpf/platforms/lisy/lisy.py
@@ -148,8 +148,9 @@ class LisyDisplay(SegmentDisplaySoftwareFlashPlatformInterface):
             # display info for display
             display_info = yield from (self.platform.send_byte_and_read_response(
                 LisyDefines.InfoGetDisplayDetails, bytearray([self.number]), 2))
-            if 0 > display_info[1] > 6:
-                raise AssertionError("Invalid display type {} in hardware".format(self._type_of_display))
+            if 1 > display_info[1] > 6:
+                raise AssertionError("Invalid display type {} reported by hardware for display {}".format(
+                    self._type_of_display, self.number))
 
             self._length_of_display = int(display_info[0])
             self._type_of_display = display_info[1]
