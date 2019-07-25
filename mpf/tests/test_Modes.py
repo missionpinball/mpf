054a0b92d3dd8565fe959b17033a151e6b3d4038
Pinki Mansukhani <47509095+pmansukhani@users.noreply.github.com>
2019-07-24 17:01:20 -0700

    Enable users to organize modes in subfolders

--- mpf/tests/test_Modes.py
+++ mpf/tests/test_Modes.py
@@ -19,6 +19,9 @@ class TestModes(MpfFakeGameTestCase):
         self.assertIn('mode2', self.machine.modes)
         self.assertIn('mode3', self.machine.modes)
         self.assertIn('mode4', self.machine.modes)
+        self.assertIn('mode5', self.machine.modes)
+        self.assertIn('mode6', self.machine.modes)
+        self.assertIn('mode7', self.machine.modes)
 
     def test_mode_start_stop(self):
         # Setup mocked event handlers for mode start/stop sequence
