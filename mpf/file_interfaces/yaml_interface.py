17b20a02003a4d129e7ec2f817a0b6081e921543
Jan Kantert <jan-mpf@kantert.net>
2019-12-20 15:30:12 +0100

    remove unused import

--- mpf/file_interfaces/yaml_interface.py
+++ mpf/file_interfaces/yaml_interface.py
@@ -13,7 +13,7 @@ from collections.abc import Hashable
 import ruamel.yaml as yaml  # pylint: disable-msg=useless-import-alias
 from ruamel.yaml.error import MarkedYAMLError
 from ruamel.yaml.reader import Reader
-from ruamel.yaml.resolver import BaseResolver, Resolver
+from ruamel.yaml.resolver import BaseResolver
 from ruamel.yaml.scanner import Scanner
 from ruamel.yaml.parser import Parser
 from ruamel.yaml.composer import Composer
