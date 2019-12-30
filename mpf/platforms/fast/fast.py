5311efc14ab176c867c6e61c244ba203992ade2a
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2019-12-29 16:10:49 -0800

    Fix linter

--- mpf/platforms/fast/fast.py
+++ mpf/platforms/fast/fast.py
@@ -862,11 +862,11 @@ class FastHardwarePlatform(ServoPlatform, LightsPlatform, DmdPlatform,
         ignore_rgb = self.config['ignore_rgb_crash'] and \
             remote_processor == self.rgb_connection.remote_processor
         if msg in ('00', '02'):
+            action = "Ignoring RGB crash and continuing play." if ignore_rgb else "MPF will exit now."
             self.error_log("The FAST {} processor rebooted. Unfortunately, that means that it lost all it's state "
                            "(such as hardware rules or switch configs). This is likely cause by an unstable power "
-                           "supply but it might as well be a firmware bug. {}".format(
-                                remote_processor,
-                                "Ignoring RGB crash and continuing play." if ignore_rgb else "MPF will exit now."))
+                           "supply but it might as well be a firmware bug. {}"
+                           .format(remote_processor, action))
             if ignore_rgb:
                 self.machine.events.post("fast_rgb_rebooted", msg=msg)
                 return
