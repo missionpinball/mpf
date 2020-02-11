501c200388174a39538bb7d984de05e51c2021fa
Jan Kantert <jan-mpf@kantert.net>
2020-02-11 14:02:42 +0100

    add a test

--- mpf/tests/test_BallDeviceModernTroughPlungerSetup.py
+++ mpf/tests/test_BallDeviceModernTroughPlungerSetup.py
@@ -27,3 +27,59 @@ class TestBallDeviceModernTroughPlungerSetup(MpfTestCase):
         self.assertEqual("idle", self.machine.ball_devices["bd_plunger"].state)
         self.assertBallsOnPlayfield(2)
         self.assertNumBallsKnown(4)
+
+
+class TestBallDeviceModernTroughPlungerSetupRaces(MpfTestCase):
+
+    def get_platform(self):
+        return "virtual"
+
+    def get_config_file(self):
+        return 'test_modern_trough_plunger_setup.yaml'
+
+    def get_machine_path(self):
+        return 'tests/machine_files/ball_device/'
+
+    def test_ball_in_plunger_during_eject(self):
+        # add some initial balls
+        self.mock_event("balldevice_bd_trough_ejecting_ball")
+        self.mock_event("balldevice_bd_plunger_ejecting_ball")
+        self.mock_event("balldevice_bd_trough_ball_eject_success")
+        self.mock_event("balldevice_bd_plunger_ball_eject_success")
+        self.hit_switch_and_run("s_trough_switch1", 0)
+        self.hit_switch_and_run("s_trough_switch2", 0)
+        self.hit_switch_and_run("s_trough_switch3", 1)
+        self.advance_time_and_run(10)
+        self.assertNumBallsKnown(3)
+
+        self.machine.playfield.add_ball()
+        self.advance_time_and_run(.3)
+        self.assertEventCalled("balldevice_bd_trough_ejecting_ball")
+        self.mock_event("balldevice_bd_trough_ejecting_ball")
+        self.assertEventNotCalled("balldevice_bd_plunger_ejecting_ball")
+        self.release_switch_and_run("s_trough_switch3", 1)
+
+        self.hit_switch_and_run("s_ball_switch_plunger_lane", 0)
+        self.advance_time_and_run(10)
+        self.assertEventNotCalled("balldevice_bd_trough_ejecting_ball")
+        self.assertEventCalled("balldevice_bd_plunger_ejecting_ball")
+        self.mock_event("balldevice_bd_plunger_ejecting_ball")
+
+        self.release_switch_and_run("s_ball_switch_plunger_lane", 0)
+        self.advance_time_and_run(3)
+
+        self.assertEventNotCalled("balldevice_bd_trough_ejecting_ball")
+        self.assertEventNotCalled("balldevice_bd_plunger_ejecting_ball")
+
+        self.hit_and_release_switch("s_playfield")
+        self.advance_time_and_run(20)
+
+        self.assertEventNotCalled("balldevice_bd_trough_ejecting_ball")
+        self.assertEventNotCalled("balldevice_bd_plunger_ejecting_ball")
+        self.assertEventCalled("balldevice_bd_trough_ball_eject_success", 1)
+        self.assertEventCalled("balldevice_bd_plunger_ball_eject_success", 1)
+
+        self.assertEqual("idle", self.machine.ball_devices["bd_trough"].state)
+        self.assertEqual("idle", self.machine.ball_devices["bd_plunger"].state)
+        self.assertBallsOnPlayfield(1)
+        self.assertNumBallsKnown(3)
