8fb214a6862c8a95ffd579a873a9693197cb8b9c
Jan Kantert <jan-mpf@kantert.net>
2020-08-07 20:21:08 +0200

    test player_controlled_eject

--- mpf/tests/test_BallDevice_SmartVirtual.py
+++ mpf/tests/test_BallDevice_SmartVirtual.py
@@ -1,7 +1,8 @@
-from mpf.tests.MpfTestCase import MpfTestCase
+from mpf.tests.MpfGameTestCase import MpfGameTestCase
+from mpf.tests.MpfTestCase import test_config
 
 
-class TestBallDeviceSmartVirtual(MpfTestCase):
+class TestBallDeviceSmartVirtual(MpfGameTestCase):
     def get_config_file(self):
         return 'test_ball_device.yaml'
 
@@ -42,3 +43,14 @@ class TestBallDeviceSmartVirtual(MpfTestCase):
         # all balls should be gone
         self.assertEqual(0, self.machine.ball_devices["test_trough"].balls)
         self.assertEqual(0, self.machine.ball_devices["test_trough"].available_balls)
+
+    @test_config("test_player_controlled_eject.yaml")
+    def test_player_controlled_eject(self):
+        self.start_game()
+        self.advance_time_and_run(10)
+        self.assertBallsInPlay(1)
+        self.assertBallsOnPlayfield(0)
+
+        self.hit_and_release_switch("s_launch_button")
+        self.advance_time_and_run(5)
+        self.assertBallsOnPlayfield(1)
