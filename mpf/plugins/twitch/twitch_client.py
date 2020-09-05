1f84992f1cdcb26c233c2f92d93544a2b9bfc663
Mark Seiden <mseiden@atlantistech.com>
2020-09-05 11:36:20 -0400

    Fix initialization

--- mpf/plugins/twitch/twitch_client.py
+++ mpf/plugins/twitch/twitch_client.py
@@ -26,7 +26,7 @@ class TwitchClient(irc.bot.SingleServerIRCBot):
         server = 'irc.chat.twitch.tv'
         port = 6667
         self.log.info('Connecting to ' + server + ' on port ' + str(port) + '...')
-        super().__init__(self, [(server, port, 'oauth:' + password)], username, username)
+        super().__init__([(server, port, 'oauth:' + password)], username, username)
         # self.connection.add_global_handler("all_events", self.on_all_events, -100)
 
     def on_welcome(self, c, e):
