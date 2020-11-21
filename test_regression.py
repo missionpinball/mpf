f6259f33f365bf04fdf5cf1252ca609f7f901b71
Jan Kantert <jan-mpf@kantert.net>
2020-11-21 17:23:19 +0100

    do not accidentially run regression tests

--- test_regression.py
+++ test_regression.py
@@ -5,20 +5,22 @@ import unittest
 
 from mpf.tests.MpfDocTestCase import MpfDocTestCase
 
-tests = []
-for subdir, dirs, files in os.walk(os.path.join(os.path.dirname(__file__), "mpf", "tests", "regression_tests")):
-    for file in files:
-        full_path = os.path.join(subdir, file)
-        with open(full_path) as f:
-            config_string = f.read()
-        testcase = MpfDocTestCase(config_string)
-        testcase._testMethodDoc = full_path
-        tests.append(testcase)
+if __name__ == '__main__':
 
-suite = unittest.TestSuite()
-suite.addTests(tests)
-result = unittest.TextTestRunner(verbosity=1).run(suite)
-if result.wasSuccessful():
-    sys.exit(0)
-else:
-    sys.exit(1)
+    tests = []
+    for subdir, dirs, files in os.walk(os.path.join(os.path.dirname(__file__), "mpf", "tests", "regression_tests")):
+        for file in files:
+            full_path = os.path.join(subdir, file)
+            with open(full_path) as f:
+                config_string = f.read()
+            testcase = MpfDocTestCase(config_string)
+            testcase._testMethodDoc = full_path
+            tests.append(testcase)
+
+    suite = unittest.TestSuite()
+    suite.addTests(tests)
+    result = unittest.TextTestRunner(verbosity=1).run(suite)
+    if result.wasSuccessful():
+        sys.exit(0)
+    else:
+        sys.exit(1)
