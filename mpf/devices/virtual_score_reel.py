69b3f478c04d1049be005c0654c864b2c4a14820
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2020-12-05 19:42:59 -0800

    Config param 'reel_count' for virtual score reels

--- mpf/devices/virtual_score_reel.py
+++ mpf/devices/virtual_score_reel.py
@@ -11,11 +11,13 @@ class VirtualScoreReel(SystemWideDevice):
   def __init__(self, machine, name):
     super().__init__(machine, name)
     self._frames = {}
+    self._reel_count = 0
 
   async def _initialize(self):
     await super()._initialize()
     self.machine.log.info("Initializing virtual score reel {}: {}".format(self.name, self.config))
 
+    self._reel_count = self.config["reel_count"]
     for frame in self.config["frames"]:
       self._frames[str(frame["character"])] = str(frame["frame"])
 
@@ -24,6 +26,6 @@ class VirtualScoreReel(SystemWideDevice):
 
   def _post_reel_values(self, **kwargs):
     names = ["1", "10", "100", "1k", "10k", "100k", "1m", "10m", "100m", "1b", "10b", "100b", "1t"]
-    score = str(kwargs["value"])[::-1]
-    result = { names[i]: self._frames[score[i]] for i in range(len(score))}
+    score = str(kwargs["value"])[::-1].ljust(self._reel_count, "0")
+    result = { names[i]: self._frames[score[i]] for i in range(self._reel_count)}
     self.machine.events.post("player{}_score_reel".format(self.machine.game.player.number), **result)
