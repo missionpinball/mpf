26ad3847e536c6297bf497a1794f1ef8b443dfc0
Mark Green <drmoose94@gmail.com>
2020-12-30 20:55:34 +0000

    Initial try

---
+++ mpf/commands/wire.py
@@ -0,0 +1,67 @@
+"""Command to start the MPF game engine."""
+
+import argparse
+import errno
+import os
+import signal
+import socket
+import sys
+from datetime import datetime
+import logging
+from logging.handlers import QueueHandler, SysLogHandler
+
+from queue import Queue
+
+from mpf.core.machine import MachineController
+from mpf.core.utility_functions import Util
+from mpf.core.config_loader import YamlMultifileConfigLoader, ProductionConfigLoader
+from mpf.commands.logging_formatters import JSONFormatter
+
+
+class Command:
+
+    """Runs the mpf game."""
+
+    # pylint: disable-msg=too-many-locals,too-many-statements
+    def __init__(self, mpf_path, machine_path, args):
+        """Run mpf game."""
+        del mpf_path
+        self.machine = None
+        self._sigint_count = 0
+
+        parser = argparse.ArgumentParser(
+            description='Generates wiring .yaml files')
+
+        parser.add_argument("-c",
+                    action="store", dest="configfile",
+                    default="config.yaml", metavar='config_file',
+                    help="The name of a config file to load. Default "
+                         "is "
+                         "config.yaml. Multiple files can be used "
+                         "via a comma-"
+                         "separated list (no spaces between)")
+
+        self.args = parser.parse_args(args)
+        self.args.configfile = Util.string_to_event_list(self.args.configfile)
+
+        self.args.__dict__["production"] = False
+        self.args.__dict__["force_platform"] = None
+        self.args.__dict__["text_ui"] = False
+        self.args.__dict__["bcp"] = False
+
+
+        config_loader = YamlMultifileConfigLoader(machine_path, self.args.configfile,
+                                                      False, False)
+
+        config = config_loader.load_mpf_config()
+
+        print(config.get_machine_config())
+
+        self.machine = MachineController(vars(self.args), config)
+        self.machine.initialise_mpf()
+
+        for l in self.machine.lights:
+            print(l.name)
+
+        # pylint: disable-msg=broad-except
+
