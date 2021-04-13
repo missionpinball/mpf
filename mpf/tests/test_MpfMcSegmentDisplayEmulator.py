577fa50a4928db6be68c92e8b3361fc1dcfcd3ad
Quinn Capen <qcapen@gmail.com>
2021-04-13 13:32:53 -0600

    Add additional test

--- mpf/tests/test_MpfMcSegmentDisplayEmulator.py
+++ mpf/tests/test_MpfMcSegmentDisplayEmulator.py
@@ -18,6 +18,7 @@ class TestMpfMcSegmentDisplayEmulatorPlatform(MpfBcpTestCase):
     def testSegmentDisplay(self):
         # Test triggers and the trigger player which is used to send trigger messages from MPF over BCP
         client = self.machine.bcp.transport.get_named_client("local_display")
+        self.assertIsNotNone(client)
 
         self.machine.segment_displays["display1"].add_text("1337", key="score")
         self.advance_time_and_run()
@@ -28,3 +29,12 @@ class TestMpfMcSegmentDisplayEmulatorPlatform(MpfBcpTestCase):
         self.advance_time_and_run()
         queue = self._bcp_external_client.reset_and_return_queue()
         self.assertIn(("trigger", {"name": "update_segment_display_1_color", "color": RGBColor("FF0000")}), queue)
+
+    def testSegmentDisplayPlayer(self):
+        client = self.machine.bcp.transport.get_named_client("local_display")
+        self.assertIsNotNone(client)
+
+        self.machine.events.post("update_display2")
+        self.advance_time_and_run()
+        queue = self._bcp_external_client.reset_and_return_queue()
+        self.assertIn(("trigger", {"name": "update_segment_display_2_text", "text": "NEW TEXT"}), queue)
