8d72a9676b6bbb18c23f3d6322c29d516a0b388f
Jan Kantert <jan-mpf@kantert.net>
2021-04-21 23:31:17 +0200

    improve credit audits - thanks James

--- mpf/tests/test_ServiceMode.py
+++ mpf/tests/test_ServiceMode.py
@@ -153,7 +153,7 @@ class TestServiceMode(MpfFakeGameTestCase):
         self.hit_and_release_switch("s_service_esc")
         self.advance_time_and_run(.1)
         self.assertMachineVarEqual(2, "credit_units")
-        self.assertEqual({'1 Total Coins service_credit': 2, '2 Total Earnings service_credit': 2},
+        self.assertEqual({'service_credit Awards': 2},
                          self.machine.modes["credits"].earnings)
 
         self.start_game()
