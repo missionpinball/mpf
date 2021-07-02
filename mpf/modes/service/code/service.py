91bf567418b6b4dfbd2ede5de72b38822b9be3c3
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2021-07-02 12:29:06 -0700

    Fix pct sign in Light service menu, update tests

--- mpf/modes/service/code/service.py
+++ mpf/modes/service/code/service.py
@@ -360,10 +360,11 @@ software_update_script: single|str|None
 
     def _update_light_slide(self, items, position, color):
         board, light = items[position]
+        label_string = "" if light.config['label'] == "%" else light.config['label']
         self.machine.events.post("service_light_test_start",
                                  board_name=board,
                                  light_name=light.name,
-                                 light_label=light.config['label'],
+                                 light_label=label_string,
                                  light_num=light.config['number'],
                                  test_color=color)
 
