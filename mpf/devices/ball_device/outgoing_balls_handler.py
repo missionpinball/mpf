0f4dd30677d5d90c70659e2fa520ca3942065b7e
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2023-05-20 14:45:50 -0700

    Multiball lock ball handler fixes for multiple-eject devices

--- mpf/devices/ball_device/outgoing_balls_handler.py
+++ mpf/devices/ball_device/outgoing_balls_handler.py
@@ -424,6 +424,25 @@ class OutgoingBallsHandler(BallDeviceStateHandler):
             result = await self._handle_confirm(eject_request, ball_eject_process, incoming_ball_at_target,
                                                 eject_try)
             await self.ball_device.ball_count_handler.end_eject(ball_eject_process, result)
+
+            self.info_log("Eject successful, looking for missing balls")
+            new_balls = await self.ball_device.ball_count_handler.counter.count_balls()
+            old_balls = self.ball_device.counted_balls
+            self.info_log("Found %s physical balls and %s expected balls", new_balls, old_balls)
+            if new_balls < old_balls:
+                # Post that the ball is lost
+                await self.ball_device.lost_idle_ball()
+                # Cancel the eject queue for the lost ball
+                self.info_log("How many ejects are requested? %s", self.ball_device.requested_balls)
+                self.info_log("Playfield ejects requested? %s", self.machine.playfields.playfield.num_balls_requested)
+                for _ in range(0, old_balls - new_balls):
+                    self.info_log("Cancelling one queued request")
+                    self._eject_queue.get_nowait()
+                    self._eject_queue.task_done()
+                self.info_log("Necessary queue requests are completed. Updating ball count to %s." % new_balls)
+                self.ball_device.ball_count_handler._set_ball_count(new_balls)
+
+
             return result
         except asyncio.CancelledError:
             ball_eject_process.cancel()
