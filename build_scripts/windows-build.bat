3c6d7415af6e745917b45c73b7ae00d7d831a2f7
Brian Madden <brian@fastpinball.com>
2023-10-05 12:01:20 -0700

    Removed very old build scripts
    The GitHub workflows have examples of how to build MPF. It's simple since there's nothing to compile.

--- build_scripts/windows-build.bat
+++
@@ -1,8 +0,0 @@
-python -m pip install -U setuptools wheel pip mock --retries 20 --timeout 60
-git clone file:///z/git/mpf c:\mpf
-c:
-cd /mpf
-pip install . --retries 20 --timeout 60
-python -m unittest discover -s mpf/tests
-python setup.py bdist_wheel --dist-dir=%~dp0%/dist
-python setup.py sdist --dist-dir=%~dp0%/dist  --formats=gztar
