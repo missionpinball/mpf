b260572248631a5ec66792684c3a025aa972542c
Brian Madden <brian@fastpinball.com>
2023-10-19 11:40:48 +0200

    FAST: refactored audio interface internals

--- mpf/platforms/fast/fast_audio.py
+++ mpf/platforms/fast/fast_audio.py
@@ -6,33 +6,48 @@ class FASTAudioInterface(LogMixin):
 
     """Hardware sound system using the FAST Audio board."""
 
-    __slots__ = ["platform", "communicator", "current_volume", "current_headphones_volume", "temp_ducked_volume_steps",
-                 "friendly_max_volume", "friendly_max_headphone_volume", "volume_map", "headphones_map",
-                 "link_phones_to_main"]
+    __slots__ = ["platform", "communicator", "amps", "temp_ducked_volume_steps"]
 
     def __init__(self, platform, communicator):
         self.platform = platform
         self.communicator = communicator
 
-        self.current_volume = 0
-        self.current_headphones_volume = 0
-        self.temp_ducked_volume_steps = 0
-        self.friendly_max_volume = communicator.config['friendly_max_volume']
-        self.friendly_max_headphone_volume = communicator.config['friendly_max_headphone_volume']
-        self.volume_map = list()
-        self.headphones_map = list()
+        self.amps = {'main': {}, 'sub': {}, 'headphones': {}}
 
-        # changes phones with main volume, useful for line out, 3rd amp, etc.
-        self.link_phones_to_main = communicator.config['link_phones_to_main']
+        for amp in self.amps.keys():
 
-        # Just set everything here. The communicator will update the
-        # config on its own as part of its init process
-        if communicator.config['main_amp_enabled']:
-            communicator.enable_amp('main', send_now=False)
-        if communicator.config['sub_amp_enabled']:
-            communicator.enable_amp('sub', send_now=False)
-        if communicator.config['headphones_amp_enabled']:
-            communicator.enable_amp('headphones', send_now=False)
+            self.amps[amp]['steps'] = communicator.config[f'{amp}_steps']
+            self.amps[amp]['max_volume'] = communicator.config[f'max_volume_{amp}']
+            self.amps[amp]['levels_list'] = communicator.config[f'{amp}_levels_list']
+            self.amps[amp]['link_to_main'] = communicator.config[f'link_{amp}_to_main']
+
+            # Just set everything here. The communicator will update the
+            # config on its own as part of its init process
+            if communicator.config[f'{amp}_amp_enabled']:
+                communicator.enable_amp(amp, send_now=False)
+
+            if not self.amps[amp]['levels_list']:
+                self._create_levels_list(amp)
+            else:
+                # if we have a levels list in the config, make sure the steps num is right
+                self.amps[amp]['steps'] = len(self.amps[amp]['levels_list']) - 1
+
+            # TODO read in machine vars, fall back to defaults if not set
+            self.amps[amp]['volume'] = communicator.config['default_main_volume']
+
+            if self.amps[amp]['link_to_main']:
+                if len(self.amps[amp]['levels_list']) != len(self.amps['main']['levels_list']):
+                    raise AssertionError(f"Invalid {amp}_levels_list. Must be same length as main_levels_list")
+
+                self.amps[amp]['volume'] = self.amps['main']['volume']
+            else:
+                self.amps[amp]['volume'] = communicator.config[f'default_{amp}_volume']
+
+            self.communicator.set_volume(amp, self.amps[amp]['volume'], send_now=False)
+
+            # TODO register for machine var volume changes & other control events
+
+        # Set audio board config
 
         communicator.set_phones_level(communicator.config['headphones_level'], send_now=False)
 
@@ -41,134 +56,64 @@ class FASTAudioInterface(LogMixin):
         else:
             phones = 'ignore'
 
-        # TODO register for master volume machine variable changes
-        # TODO register for headphone volume changes & other control events
-
-        self.volume_map = communicator.config['volume_map']
-        if not self.volume_map:
-            self._create_volume_map()
-        else:
-            self.friendly_max_volume = len(self.volume_map) - 1
-
-        if not self.headphones_map:
-            self._create_headphones_map()
-        else:
-            self.friendly_max_headphone_volume = len(self.headphones_map) - 1
+        self.temp_ducked_volume_steps = 0
 
         communicator.set_phones_behavior(phones, send_now=False)
 
-        # TODO read in machine vars, fall back to defaults if not set
-        self.current_volume = communicator.config['default_volume']
-
-        if self.link_phones_to_main:
-            self.current_headphones_volume = self.current_volume
-        else:
-            self.current_headphones_volume = communicator.config['default_headphones_volume']
-
-        self.communicator.set_volume('main', self.volume_map[self.current_volume][0], send_now=False)
-        self.communicator.set_volume('sub', self.volume_map[self.current_volume][1], send_now=False)
-        self.communicator.set_volume('headphones', self.headphones_map[self.current_headphones_volume], send_now=False)
-
-    def _create_volume_map(self):
-        # volume_map is a list of tuples, each tuple is (main_volume, sub_volume)
-        # the list index is the friendly volume level, e.g. 0-20
-        # volume_map[0] will always be (0,0)
-        # the highest will always be (max_volume_main, max_volume_sub)
-        # valid values in the map are 0-63. These are the technical hardware
-        # volume levels. Users will see the friendly volume levels, e.g. 0-20
-        steps = self.communicator.config['friendly_max_volume']
-        max_volume_main = self.communicator.config['max_volume_main']
-        max_volume_sub = self.communicator.config['max_volume_sub']
-        sub_percent_of_main = max_volume_sub / max_volume_main
-        step_percent = 1 / steps
-
-        if  max_volume_main > 63:
-            raise AssertionError(f"Invalid max_volume_main: {max_volume_main}")
-        if max_volume_sub > 63:
-            raise AssertionError(f"Invalid max_volume_sub: {max_volume_sub}")
-
-        # calculate the sub and main volume for each step
-        for i in range(steps):
-            main_volume = ceil(max_volume_main * (1 - (i * step_percent)))
-            sub_volume = ceil(main_volume * sub_percent_of_main)
-            self.volume_map.append((main_volume, sub_volume))
-
-        # technically our list is number of volume levels + 1, but that's more logicial
-        # for humans. e.g. 20 volume steps of 5% each is 0-100% volume, but
-        # it's 21 items in the list.
-        self.volume_map.append((0, 0))
-        self.volume_map.reverse()
-
-    def _create_headphones_map(self):
-        steps = self.communicator.config['friendly_max_headphone_volume']
-        max_volume = self.communicator.config['max_volume_headphones']
+    def _create_levels_list(self, amp):
+        steps = self.communicator.config[f'{amp}_steps']
+        max_volume = self.communicator.config[f'max_volume_{amp}']
         step_percent = 1 / steps
 
         if max_volume > 63:
-            raise AssertionError(f"Invalid max_volume_headphones: {max_volume}")
+            raise AssertionError(f"Invalid max_volume_{amp}: {max_volume}")
 
         for i in range(steps):
             volume = ceil(max_volume * (1 - (i * step_percent)))
-            self.headphones_map.append(volume)
+            self.amps[amp]['levels_list'].append(volume)
 
-        self.headphones_map.append(0)
-        self.headphones_map.reverse()
+        # technically our list is number of volume levels + 1, since that's more logicial
+        # for humans. e.g. 20 volume steps of 5% each is 0-100% volume, which
+        # has 21 items in the list.
+        self.amps[amp]['levels_list'].append(0)
+        self.amps[amp]['levels_list'].reverse()
 
-    def send_volume_to_hw(self):
-        self.communicator.set_volume('main', self.volume_map[self.current_volume][0])
-        self.communicator.set_volume('sub', self.volume_map[self.current_volume][1])
+    def send_volume_to_hw(self, amp=None):
+        """Send the current volume to the hardware."""
+        if amp is None:
+            for amp in self.amps.keys():
+                self.send_volume_to_hw(amp)
+            return
 
-    def send_headphones_volume_to_hw(self):
-        self.communicator.set_volume('headphones', self.headphones_map[self.current_headphones_volume])
+        self.communicator.set_volume(amp, self.amps[amp]['volume'])
 
-    def increase_volume(self, steps=1, **kwargs):
+    def increase_volume(self, amp, steps=1, **kwargs):
         """Increase the volume by the specified number of steps."""
-        self.set_volume(self.current_volume + int(steps))
+        self.set_volume(amp, self.amps[amp]['volume'] + int(steps))
 
-    def decrease_volume(self, steps=1, **kwargs):
+    def decrease_volume(self, amp, steps=1, **kwargs):
         """Decrease the volume by the specified number of steps."""
-        self.set_volume(self.current_volume - int(steps))
+        self.set_volume(amp, self.amps[amp]['volume'] - int(steps))
 
-    def set_volume(self, volume=0, **kwargs):
-        """Set the main/sub volume to the specified level, using the volume map"""
+    def set_volume(self, amp, volume=0, **kwargs):
+        """Set the amp volume to the specified level, using the volume map"""
         volume = int(volume)
 
-        if volume > self.friendly_max_volume:
-            volume = self.friendly_max_volume
+        if volume > self.amps[amp]['steps']:
+            volume = self.amps[amp]['steps']
         elif volume < 0:
             volume = 0
 
-        self.current_volume = volume
-        self.send_volume_to_hw()
+        self.amps[amp]['volume'] = volume
+        self.send_volume_to_hw(amp)
 
-        if self.link_phones_to_main:
-            self.set_headphones_volume(volume)
+        for other_amp in self.amps.keys():
+            if self.amps[other_amp]['link_to_main'] and other_amp != amp:
+                self.amps[other_amp]['volume'] = volume
+                self.send_volume_to_hw(other_amp)
 
-    def increase_headphones_volume(self, steps=1, **kwargs):
-        """Increase the headphones volume by the specified number of steps."""
-        self.set_headphones_volume(self.current_headphones_volume + int(steps))
-
-    def decrease_headphones_volume(self, steps=1, **kwargs):
-        """Decrease the headphones volume by the specified number of steps."""
-        self.set_headphones_volume(self.current_headphones_volume - int(steps))
-
-    def set_headphones_volume(self, volume=0, **kwargs):
-        """Set the headphones volume to the specified level using the
-        headphones map"""
-
-        if self.link_phones_to_main:
-            return False  # use set_volume() instead
-
-        volume = int(volume)
-
-        if volume > self.friendly_max_headphone_volume:
-            volume = self.friendly_max_headphone_volume
-        elif volume < 0:
-            volume = 0
-
-        self.current_headphones_volume = volume
-        self.send_headphones_volume_to_hw()
-        return True
+        # TODO write new machine var (verify if it's the same value that it
+        # won't re-post)
 
     def temp_duck_volume(self, steps=1, **kwargs):
         """Temporarily duck the volume by the specified number of steps.
@@ -177,11 +122,11 @@ class FASTAudioInterface(LogMixin):
         """
         steps = int(steps)
         self.temp_ducked_volume_steps = steps
-        self.set_volume(self.current_volume - steps)
+        self.set_volume('main', self.amps['main']['volume'] - steps)
 
     def restore_volume(self, **kwargs):
-        """Restore the volume to the value it was before the last"""
-        self.set_volume(self.current_volume + self.temp_ducked_volume_steps)
+        """Restore the volume to the value it was before it was ducked."""
+        self.set_volume(self.amps['main']['volume'] + self.temp_ducked_volume_steps)
 
     def pulse_lcd_pin(self, pin, ms=100, **kwargs):
         """Pulse the specified LCD pin for the specified number of milliseconds."""
