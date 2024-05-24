abecfa3424301215ddf9d7974ca2a7a11baf59c2
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-05-24 10:41:41 -0700

    Update tests for Carousel, Bonus

--- mpf/tests/test_CarouselMode.py
+++ mpf/tests/test_CarouselMode.py
@@ -24,34 +24,27 @@ class TestCarouselMode(MpfTestCase):
         self.assertIsNone(self.machine.game)
 
     def testBlockingCarousel(self):
-        self.mock_event("blocking_carousel_item1_highlighted")
-        self.mock_event("blocking_carousel_item2_highlighted")
-        self.mock_event("blocking_carousel_item3_highlighted")
+        self.mock_event("item_highlighted")
         self.mock_event("flipper_cancel")
-        
+
         self._start_game()
         self.post_event("start_mode4")
 
         self.assertIn(self.machine.modes["blocking_carousel"], self.machine.mode_controller.active_modes)
-        self.assertEqual(1, self._events["blocking_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["blocking_carousel_item2_highlighted"]) 
+        self.assertEventCalledWith("item_highlighted", carousel="blocking_carousel", item="item1", direction=None)
         self.post_event("s_flipper_right_active")
         self.post_event("s_flipper_right_inactive")
-        self.assertEqual(1, self._events["blocking_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["blocking_carousel_item3_highlighted"]) 
-        self.assertEqual(0, self._events["flipper_cancel"]) 
+        self.assertEventCalledWith("item_highlighted", carousel="blocking_carousel", item="item2", direction="forwards")
+        self.assertEqual(0, self._events["flipper_cancel"])
         self.post_event("s_flipper_right_active")
         self.post_event("s_flipper_left_active")
         self.post_event("flipper_cancel")
         self.post_event("s_flipper_right_inactive")
         self.post_event("s_flipper_left_inactive")
         self.assertEqual(1, self._events["flipper_cancel"])
-        self.assertEqual(1, self._events["blocking_carousel_item1_highlighted"])
-        self.assertEqual(1, self._events["blocking_carousel_item2_highlighted"]) 
-        self.assertEqual(0, self._events["blocking_carousel_item3_highlighted"]) 
         self.post_event("both_flippers_inactive")
         self.post_event("s_flipper_right_inactive")
-        self.assertEqual(1, self._events["blocking_carousel_item3_highlighted"]) 
+        self.assertEventCalledWith("item_highlighted", carousel="blocking_carousel", item="item3", direction="forwards")
 
         # Restart the mode to ensure that the block is cleared
         self.post_event("flipper_cancel")
@@ -59,78 +52,56 @@ class TestCarouselMode(MpfTestCase):
         self.advance_time_and_run()
         self.post_event("start_mode4")
         self.post_event("s_flipper_right_inactive")
-        self.assertEqual(2, self._events["blocking_carousel_item2_highlighted"], 
-            "item2_highlighted should be called when a blocked mode restarts") 
+        # item2 highlighted should be called when a blocked mode restarts
+        self.assertEventCalledWith("item_highlighted", carousel="blocking_carousel", item="item2", direction="forwards")
+
 
     def testConditionalCarousel(self):
-        self.mock_event("conditional_carousel_item1_highlighted")
-        self.mock_event("conditional_carousel_item2_highlighted")
-        self.mock_event("conditional_carousel_item3_highlighted")
-        self.mock_event("conditional_carousel_item4_highlighted")
+        self.mock_event("item_highlighted")
 
         self._start_game()
 
         # Start the mode without any conditions true
         self.post_event("start_mode3")
         self.assertIn(self.machine.modes["conditional_carousel"], self.machine.mode_controller.active_modes)
-        self.assertEqual(1, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction=None)
         self.post_event("next")
-        self.assertEqual(2, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction="forwards")
         self.post_event("next")
-        self.assertEqual(3, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction="forwards")
         self.post_event("stop_mode3")
 
         # Reset the count for item 1
-        self.mock_event("conditional_carousel_item1_highlighted")
+        self.mock_event("item_highlighted")
         # Start the mode with a player variable condition
         self.machine.game.player["show_item4"] = True
         self.post_event("start_mode3")
-        self.assertEqual(1, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction=None)
         self.post_event("next")
-        self.assertEqual(1, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(1, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item4", direction="forwards")
         self.post_event("next")
-        self.assertEqual(2, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(1, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction="forwards")
         self.post_event("stop_mode3")
 
         # Reset the count for items 1 and 4
-        self.mock_event("conditional_carousel_item1_highlighted")
-        self.mock_event("conditional_carousel_item4_highlighted")
+        self.mock_event("item_highlighted")
         # Start the mode with a machine variable condition
         self.machine.variables.set_machine_var("player2_score", 500000)
         self.machine.game.player["show_item4"] = False
         self.post_event("start_mode3")
-        self.assertEqual(1, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction=None)
         self.post_event("next")
-        self.assertEqual(1, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(1, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item3", direction="forwards")
         self.post_event("next")
-        self.assertEqual(2, self._events["conditional_carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item2_highlighted"])
-        self.assertEqual(1, self._events["conditional_carousel_item3_highlighted"])
-        self.assertEqual(0, self._events["conditional_carousel_item4_highlighted"])
+
+        self.assertEventCalledWith("item_highlighted", carousel="conditional_carousel", item="item1", direction="forwards")
         self.post_event("stop_mode3")
 
         # The mode shouldn't start if all conditions are false (i.e. no items)
@@ -142,11 +113,9 @@ class TestCarouselMode(MpfTestCase):
         self.assertNotIn(self.machine.modes["conditional_carousel"], self.machine.mode_controller.active_modes)
 
     def testExtraBall(self):
-        self.mock_event("carousel_item1_highlighted")
+        self.mock_event("item_highlighted")
         self.mock_event("carousel_item1_selected")
-        self.mock_event("carousel_item2_highlighted")
         self.mock_event("carousel_item2_selected")
-        self.mock_event("carousel_item3_highlighted")
         self.mock_event("carousel_item3_selected")
 
         # start game
@@ -155,34 +124,18 @@ class TestCarouselMode(MpfTestCase):
         self.post_event("start_mode1")
         self.assertIn(self.machine.modes["carousel"], self.machine.mode_controller.active_modes)
 
-        self.assertEqual(1, self._events["carousel_item1_highlighted"])
-        self.assertEqual(0, self._events["carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["carousel_item3_highlighted"])
-
+        self.assertEventCalledWith("item_highlighted", carousel="carousel", item="item1", direction=None)
         self.post_event("next")
-        self.assertEqual(1, self._events["carousel_item1_highlighted"])
-        self.assertEqual(1, self._events["carousel_item2_highlighted"])
-        self.assertEqual(0, self._events["carousel_item3_highlighted"])
-
+        self.assertEventCalledWith("item_highlighted", carousel="carousel", item="item2", direction="forwards")
         self.post_event("next")
-        self.assertEqual(1, self._events["carousel_item1_highlighted"])
-        self.assertEqual(1, self._events["carousel_item2_highlighted"])
-        self.assertEqual(1, self._events["carousel_item3_highlighted"])
-
+        self.assertEventCalledWith("item_highlighted", carousel="carousel", item="item3", direction="forwards")
         self.post_event("next")
-        self.assertEqual(2, self._events["carousel_item1_highlighted"])
-        self.assertEqual(1, self._events["carousel_item2_highlighted"])
-        self.assertEqual(1, self._events["carousel_item3_highlighted"])
-
+        self.assertEventCalledWith("item_highlighted", carousel="carousel", item="item1", direction="forwards")
         self.post_event("previous2")
-        self.assertEqual(2, self._events["carousel_item1_highlighted"])
-        self.assertEqual(1, self._events["carousel_item2_highlighted"])
-        self.assertEqual(2, self._events["carousel_item3_highlighted"])
-
+        self.assertEventCalledWith("item_highlighted", carousel="carousel", item="item3", direction="backwards")
         self.post_event("previous")
-        self.assertEqual(2, self._events["carousel_item1_highlighted"])
-        self.assertEqual(2, self._events["carousel_item2_highlighted"])
-        self.assertEqual(2, self._events["carousel_item3_highlighted"])
+        self.assertEventCalledWith("item_highlighted", carousel="carousel", item="item2", direction="backwards")
+
 
         self.post_event("select")
         self.assertEqual(0, self._events["carousel_item1_selected"])
