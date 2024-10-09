24596df05795b8c4f8ebfa88bf141f536eb50c47
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2024-10-09 14:41:59 -0700

    FASTStepper commands for home and stop

--- mpf/platforms/fast/fast_stepper.py
+++ mpf/platforms/fast/fast_stepper.py
@@ -2,12 +2,13 @@
 
 
 from mpf.core.utility_functions import Util
+from mpf.exceptions.config_file_error import ConfigFileError
 from mpf.platforms.interfaces.stepper_platform_interface import StepperPlatformInterface
 
 
 class FastStepper(StepperPlatformInterface):
 
-    """A servo in the FAST platform connected to a FAST Expansion Board."""
+    """A stepper in the FAST platform connected to a FAST Expansion Board."""
 
     # __slots__ = ["number", "exp_connection"]
 
@@ -17,7 +18,7 @@ class FastStepper(StepperPlatformInterface):
         self.exp_connection = breakout_board.communicator
 
         self.base_address = breakout_board.address
-        self.servo_index = str(int(port) - 1)  # Servos are 0-indexed
+        self.stepper_index = str(int(port) - 1)  # Steppers are 0-indexed
         # self.max_runtime = f"{config['max_runtime']:02X}"
 
     #     self.write_config_to_servo()
@@ -30,11 +31,15 @@ class FastStepper(StepperPlatformInterface):
 
     #     # EM:<INDEX>,1,<MAX_TIME_MS>,<MIN>,<MAX>,<NEUTRAL><CR>
     #     self.exp_connection.send_with_confirmation(
-    #         f"EM@{self.base_address}:{self.servo_index},1,{self.max_runtime},{min_us},{max_us},{home_us}",
+    #         f"EM@{self.base_address}:{self.stepper_index},1,{self.max_runtime},{min_us},{max_us},{home_us}",
     #         'EM:P')
 
     def home(self, direction):
-        pass
+        if direction != 'counterclockwise':
+            raise ConfigFileError("FAST Stepper only supports home in counter-clockwise direction. "
+                                  "Please rewire your motor and set homing_direction: counterclockwise "
+                                  "in your stepper config.", 1, self.__class__.__name__)
+        self._send_command("MH")
 
     async def wait_for_move_completed(self):
         pass
@@ -47,17 +52,20 @@ class FastStepper(StepperPlatformInterface):
         base_command = "MF" if position > 0 else "MR"
         hex_position = Util.int_to_hex_string(position, True)
 
-        # MP:<INDEX>,<POSITION>,<TIME_MS><CR>
-        self.exp_connection.send_and_forget(f'{base_command}@{self.base_address}:{self.servo_index},'
-                                            f'{hex_position}') #,{self.max_runtime}')
+        self._send_command(base_command, [hex_position])
 
-    def move_vel_mode(self, velocity):
+    def move_vel_mode(self, _velocity):
         pass
 
     def stop(self):
         """Called during shutdown."""
-        # TODO: send command to go home and power off servo
+        self._send_command("MC")
 
     def set_home_position(self):
         pass
+
+    def _send_command(self, base_command, payload=[]):
+        self.exp_connection.send_and_forget(','.join([
+            f'{base_command}@{self.base_address}:{self.stepper_index}', *payload]))
+
 
