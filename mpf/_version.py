f6a648e492f9d90067ee2b3100563f4aa6613785
Alex Lobascio <alex@Lobasc.io>
2026-01-01 15:05:14 -0800

    increment MPF version to 0.58 due to breaking change in FAST EXP firmware support

--- mpf/_version.py
+++ mpf/_version.py
@@ -10,10 +10,10 @@ PyPI.
 
 """
 
-__version__ = '0.57.4.dev3'  # Also consider whether MPF-MC pyproject.toml should be updated
+__version__ = '0.58.0.dev1'  # Also consider whether MPF-MC pyproject.toml should be updated
 '''The full version of MPF.'''
 
-__short_version__ = '0.57'
+__short_version__ = '0.58'
 '''The major.minor version of MPF.'''
 
 __bcp_version__ = '1.1'
