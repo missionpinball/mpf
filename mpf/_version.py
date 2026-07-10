2a8a1cd28b32dc27a653da7474d032430810cfa7
Bosh <alex@lobasc.io>
2026-07-10 01:52:53 -0700

    bump version 0.57.6

--- mpf/_version.py
+++ mpf/_version.py
@@ -10,7 +10,7 @@ PyPI.
 
 """
 
-__version__ = '0.57.5'  # Also consider whether MPF-MC pyproject.toml should be updated
+__version__ = '0.57.6'  # Also consider whether MPF-MC pyproject.toml should be updated
 '''The full version of MPF.'''
 
 __short_version__ = '0.57'
